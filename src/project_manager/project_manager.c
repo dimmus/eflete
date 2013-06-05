4872517740d7fcaeaf7186fadd057a7ee0f40e0f
rimmed <v.reutskiy@samsung.com>
2013-06-05 18:01:43 +0300

    Fix defect: the Project object deleted before the application terminated

--- src/project_manager/project_manager.c
+++ src/project_manager/project_manager.c
@@ -4,207 +4,202 @@
 Eina_Bool
 pm_free(Project *project)
 {
-	if(!project)
-	{
-		return EINA_FALSE;
-	}
+   if(!project) return EINA_FALSE;
 
-	free(project->name);
-	free(project->edc);
-	free(project->edj);
-	free(project->swapfile);
-	free(project->image_directory);
-	free(project->font_directory);
-	free(project->sound_directory);
-	if(project->compiler)
-	{
-		compiler_free(project->compiler);
-	}
-	if(project->decompiler)
-	{
-		decompiler_free(project->decompiler);
-	}
+   INFO ("Closed project: %s", project->name);
 
-	free(project);
+   free(project->name);
+   free(project->edc);
+   free(project->edj);
+   free(project->swapfile);
+   free(project->image_directory);
+   free(project->font_directory);
+   free(project->sound_directory);
+   if(project->compiler)
+        compiler_free(project->compiler);
+   if(project->decompiler)
+        decompiler_free(project->decompiler);
 
-	return EINA_TRUE;
+   wm_widget_list_free(project->widgets);
+
+   free(project);
+   DBG ("Project data is released.");
+
+   return EINA_TRUE;
 }
 
 Project *
 pm_project_add(const char *path,
-				const char *id, /* image directory */
-				const char *fd, /* font directory */
-				const char *sd  /* sound directory */)
+               const char *id, /* image directory */
+               const char *fd, /* font directory */
+               const char *sd  /* sound directory */)
 {
-	Project *pro;
-	char *tmp = NULL;
-	char tmp_path[PATH_MAX];
-	Eina_Array *array;
-	size_t siz, siz_tmp_path;
-	int i, j;
+   Project *pro;
+   char *tmp = NULL;
+   char tmp_path[PATH_MAX];
+   Eina_Array *array;
+   size_t siz, siz_tmp_path;
+   int i, j;
 
-	pro = calloc(1, sizeof(*pro));
+   pro = calloc(1, sizeof(*pro));
 
-	array = eina_file_split(strdup(path));
-	i = eina_array_count(array);
-	eina_strlcpy(tmp_path, "/", sizeof("/"));
-	for(j = 0; j < i - 1; j++)
-	{
-		eina_strlcat(tmp_path, eina_array_data_get(array, j), PATH_MAX);
-		eina_strlcat(tmp_path, "/", PATH_MAX);
-	}
-	DBG("%s", tmp_path);
-	siz_tmp_path = strlen(tmp_path);
+   array = eina_file_split(strdup(path));
+   i = eina_array_count(array);
+   eina_strlcpy(tmp_path, "/", sizeof("/"));
+   for (j = 0; j < i - 1; j++)
+     {
+        eina_strlcat(tmp_path, eina_array_data_get(array, j), PATH_MAX);
+        eina_strlcat(tmp_path, "/", PATH_MAX);
+     }
+   DBG("%s", tmp_path);
+   siz_tmp_path = strlen(tmp_path);
 
-	/* set project name */
-	pro->name = strdup(eina_array_data_get(array, i - 1));
-	eina_array_free(array);
-	DBG("Project name: '%s'", pro->name);
+   /* set project name */
+   pro->name = strdup(eina_array_data_get(array, i - 1));
+   eina_array_free(array);
+   DBG ("Project name: '%s'", pro->name);
 
-	/* set path to edc */
-	pro->edc = strdup(path);
+   /* set path to edc */
+   pro->edc = strdup(path);
 
-	/* FIXME: A substirng '.edj' can meet in the middle of the path */
-	tmp = strstr(pro->edc, ".edj");
-	if(tmp != NULL)
-	{
-		strncpy(tmp, ".edc", 4);
-	}
-	tmp = NULL;
+   /* FIXME: A substirng '.edj' can meet in the middle of the path */
+   tmp = strstr(pro->edc, ".edj");
+   if (tmp != NULL)
+     {
+        strncpy(tmp, ".edc", 4);
+     }
+   tmp = NULL;
 
-	/* set path to edj */
-	pro->edj = strdup(path);
+   /* set path to edj */
+   pro->edj = strdup(path);
 
-	/* FIXME: A substring '.edc' can meet in the middle of the path */
-	tmp = strstr(pro->edj, ".edc");
-	if(tmp != NULL)
-	{
-		strncpy(tmp, ".edj", 4);
-	}
-	tmp = NULL;
-	DBG("Path to edc-file: '%s'", pro->edc);
-	DBG("Path to edj-file: '%s'", pro->edj);
+   /* FIXME: A substring '.edc' can meet in the middle of the path */
+   tmp = strstr(pro->edj, ".edc");
+   if (tmp != NULL)
+     {
+        strncpy(tmp, ".edj", 4);
+     }
+   tmp = NULL;
+   DBG ("Path to edc-file: '%s'", pro->edc);
+   DBG ("Path to edj-file: '%s'", pro->edj);
 
-	/* set path to swap file */
-	siz = siz_tmp_path + strlen(".swapfile_") + strlen(pro->name);
-	pro->swapfile = calloc(siz, sizeof(char));
-	eina_strlcat(pro->swapfile, tmp_path, PATH_MAX);
-	eina_strlcat(pro->swapfile, ".swapfile_", PATH_MAX);
-	eina_strlcat(pro->swapfile, pro->name, PATH_MAX);
-	DBG("Path to swap file: '%s'", pro->swapfile);
+   /* set path to swap file */
+   siz = siz_tmp_path + strlen(".swapfile_") + strlen(pro->name);
+   pro->swapfile = calloc(siz, sizeof(char));
+   eina_strlcat(pro->swapfile, tmp_path, PATH_MAX);
+   eina_strlcat(pro->swapfile, ".swapfile_", PATH_MAX);
+   eina_strlcat(pro->swapfile, pro->name, PATH_MAX);
+   DBG ("Path to swap file: '%s'", pro->swapfile);
 
-	/* set path to image directory */
-	if(id)
-	{
-		pro->image_directory = strdup(id);
-	}
-	/* set default path to image directory */
-	else
-	{
-		siz = siz_tmp_path + strlen("images/") + 1;
-		pro->image_directory = calloc(siz, sizeof(char));
-		eina_strlcat(pro->image_directory, tmp_path, PATH_MAX);
-		eina_strlcat(pro->image_directory, "images/", PATH_MAX);
-	}
-	DBG("Path to image direcotory: '%s'", pro->image_directory);
+   /* set path to image directory */
+   if (id)
+     {
+        pro->image_directory = strdup(id);
+     }
+   /* set default path to image directory */
+   else
+     {
+        siz = siz_tmp_path + strlen("images/") + 1;
+        pro->image_directory = calloc(siz, sizeof(char));
+        eina_strlcat(pro->image_directory, tmp_path, PATH_MAX);
+        eina_strlcat(pro->image_directory, "images/", PATH_MAX);
+     }
+   DBG ("Path to image direcotory: '%s'", pro->image_directory);
 
-	/* set path to font directory */
-	if(fd)
-	{
-		pro->font_directory = strdup(fd);
-	}
-	/* set default path to font direcotry */
-	else
-	{
-		siz = siz_tmp_path + strlen("fonts/") + 1;
-		pro->font_directory = calloc(siz, sizeof(char));
-		eina_strlcat(pro->font_directory, tmp_path, PATH_MAX);
-		eina_strlcat(pro->font_directory, "fonts/", PATH_MAX);
-	}
-	DBG("Path to font direcotory: '%s'", pro->font_directory);
+   /* set path to font directory */
+   if (fd)
+     {
+        pro->font_directory = strdup(fd);
+     }
+   /* set default path to font direcotry */
+   else
+     {
+        siz = siz_tmp_path + strlen("fonts/") + 1;
+        pro->font_directory = calloc(siz, sizeof(char));
+        eina_strlcat(pro->font_directory, tmp_path, PATH_MAX);
+        eina_strlcat(pro->font_directory, "fonts/", PATH_MAX);
+     }
+   DBG("Path to font direcotory: '%s'", pro->font_directory);
 
-	/* set default path to sound directory */
-	if(sd)
-	{
-		pro->sound_directory = strdup(sd);
-	}
-	else
-	{
-		siz = siz_tmp_path + strlen("sounds/") + 1;
-		pro->sound_directory = calloc(siz, sizeof(char));
-		eina_strlcat(pro->sound_directory, tmp_path, PATH_MAX);
-		eina_strlcat(pro->sound_directory, "sounds/", PATH_MAX);
-	}
-	DBG("Path to sound direcotory: '%s'", pro->sound_directory);
+   /* set default path to sound directory */
+   if (sd)
+     {
+        pro->sound_directory = strdup(sd);
+     }
+   else
+     {
+        siz = siz_tmp_path + strlen("sounds/") + 1;
+        pro->sound_directory = calloc(siz, sizeof(char));
+        eina_strlcat(pro->sound_directory, tmp_path, PATH_MAX);
+        eina_strlcat(pro->sound_directory, "sounds/", PATH_MAX);
+     }
+   DBG ("Path to sound direcotory: '%s'", pro->sound_directory);
 
-	/* without this like widgets link is set into NULL.
-	Maybe this code (or something that looks like this one)
-	is existing somewhere else, but I didn't find it */
-	pro->widgets = wm_widget_list_new(pro->edj);
+   /* without this like widgets link is set into NULL.
+      Maybe this code (or something that looks like this one)
+      is existing somewhere else, but I didn't find it */
+   pro->widgets = wm_widget_list_new(pro->edj);
 
-	return pro;
+   return pro;
 }
 
 Project *
 pm_open_project_edc(const char *path,
-					const char *image_directory,
-					const char *font_directory,
-					const char *sound_directory)
+                    const char *image_directory,
+                    const char *font_directory,
+                    const char *sound_directory)
 {
-	Project *project;
+   Project *project;
 
-	if(!path)
-	{
-		ERR("Can not open project path is NULL!");
-		return NULL;
-	}
+   if(!path)
+     {
+        ERR("Can not open project path is NULL!");
+        return NULL;
+     }
 
-	project = pm_project_add(path,
-							image_directory,
-							font_directory,
-							sound_directory);
+   project = pm_project_add(path,
+                            image_directory,
+                            font_directory,
+                            sound_directory);
 
-	/* compile project and create swapfile */
-	project->compiler = compile(project->edc,
-								project->edj,
-								project->image_directory,
-								project->font_directory,
-								project->sound_directory);
-	if(project->compiler)
-	{
-		ecore_file_cp(project->edj, project->swapfile);
-	}
+   /* compile project and create swapfile */
+   project->compiler = compile(project->edc,
+                               project->edj,
+                               project->image_directory,
+                               project->font_directory,
+                               project->sound_directory);
+   if (project->compiler)
+     {
+        ecore_file_cp(project->edj, project->swapfile);
+     }
 
-	return project;
+   return project;
 }
 
 Project *
 pm_open_project_edj(const char *path)
 {
-	Project *project;
+   Project *project;
 
-	if(!path)
-	{
-		ERR("Can not open project path is NULL!");
-		return NULL;
-	}
+   if (!path)
+     {
+        ERR("Can not open project path is NULL!");
+        return NULL;
+     }
 
-	INFO("Open project! Path to project: '%s'.", path);
-	project = pm_project_add(path, NULL, NULL, NULL);
-	ecore_file_cp(project->edj, project->swapfile);
-	INFO("Project '%s' is open!", project->name);
+   INFO("Open project! Path to project: '%s'.", path);
+   project = pm_project_add(path, NULL, NULL, NULL);
+   ecore_file_cp(project->edj, project->swapfile);
+   INFO("Project '%s' is open!", project->name);
 
-	return project;
+   return project;
 }
 
 Eina_Bool
 pm_save_project_edj(Project *pm)
 {
-	if(!pm)
-	{
-		return EINA_FALSE;
-	}
+   if (!pm) return EINA_FALSE;
 
-	ecore_file_cp(pm->swapfile, pm->edj);
-	return EINA_TRUE;
+   ecore_file_cp(pm->swapfile, pm->edj);
+   return EINA_TRUE;
 }
--- src/project_manager/project_manager.h
+++ src/project_manager/project_manager.h
@@ -1,6 +1,14 @@
 #ifndef PROJECT_MANAGER_H
 #define PROJECT_MANAGER_H
 
+/**
+ * @defgroup ProjectManager ProjectManager
+ *
+ * It is a basic object. Project manager object consist a information
+ * about opened project. Project manager object consist the list of widgets
+ * styles @ref widget_manger.h
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif /* include config.h */
@@ -12,17 +20,26 @@
 
 struct _Project
 {
-	char *name;
-	char *edc;				// path to edc file(open/save)
-	char *edj;				// path to edj file(open/save)
-	char *swapfile;			// path to swap(work) file, need for undo and redo
-	Edje_CC *compiler;
-	Edje_DeCC *decompiler;
-	char *image_directory;	// path to image directory, for compile
-	char *font_directory;	// path to font directory, for compile
-	char *sound_directory;	// path to sound direcory, for compile
-
-	Eina_Inlist *widgets;	// list of widgets and they styles in that theme
+   /**< name of project */
+   char *name;
+   /**< path to edc file(open/save) */
+   char *edc;
+   /**< path to edj file(open/save) */
+   char *edj;
+   /**< path to swap(work) file */
+   char *swapfile;
+   /**<  */
+   Edje_CC *compiler;
+   /**<  */
+   Edje_DeCC *decompiler;
+   /**< path to image directory, for compile */
+   char *image_directory;
+   /**< path to font directory, for compile */
+   char *font_directory;
+   /**< path to sound direcory, for compile */
+   char *sound_directory;
+   /**< list of widgets and they styles in that theme */
+   Eina_Inlist *widgets;
 };
 
 typedef struct _Project Project;
@@ -32,12 +49,17 @@ typedef struct _Project Project;
  */
 Project *
 pm_open_project_edc(const char *path,
-					const char *image_directory,
-					const char *font_directory,
-					const char *sound_direcotory);
+                    const char *image_directory,
+                    const char *font_directory,
+                    const char *sound_direcotory);
 
 /**
+ * Open project from edj-file
  *
+ * @param path path to edj-file
+ * @return the Project object
+ *
+ * @ingroup ProjectManager
  */
 Project *
 pm_open_project_edj(const char *path);
@@ -50,16 +72,15 @@ pm_open_project_edj(const char *path);
 /**
  *
  */
-Eina_Bool
-pm_save_project_edj(Project *pm);
-
-/**
- *
- */
 /*TODO: Add new project, template project. pm_project_new(void) */
 
 /**
+ * Delete the Project object
  *
+ * @param project the given Project object
+ * @return EINA_TRUE - object deleted, EINA_FALSE - can not delete object
+ *
+ * @ingroup ProjectManager
  */
 Eina_Bool
 pm_free(Project *project);
