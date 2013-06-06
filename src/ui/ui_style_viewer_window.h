af7805cc3e667ac9a67ccda9a17338a8e7a638a5
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2013-06-06 17:06:05 +0300

    TextStyle codestyle and Property Container fixes

--- src/ui/ui_style_viewer_window.h
+++ src/ui/ui_style_viewer_window.h
@@ -12,39 +12,39 @@
 #include <Eina.h>
 #include "efl_tet.h"
 
-enum _type_style
+enum _Type_Style
 {
-	TAG,
-	TEXTSTYLE
+   TAG,
+   TEXTSTYLE
 };
 
-typedef enum _type_style type_style;
+typedef enum _Type_Style Type_Style;
 
 struct _Tag
 {
-	EINA_INLIST;
-	char *tag_name;
-	char *tag_value;
-	type_style __type;
+   EINA_INLIST;
+   char *tag_name;
+   char *tag_value;
+   Type_Style __type;
 };
 
 typedef struct _Tag Tag;
 
-struct _TextStyles
+struct _Text_Styles
 {
-	EINA_INLIST;
-	char *style_name;
-	Eina_Inlist *tags;
-	type_style __type;
+   EINA_INLIST;
+   char *style_name;
+   Eina_Inlist *tags;
+   Type_Style __type;
 };
 
-typedef struct _TextStyles TextStyles;
+typedef struct _Text_Styles Text_Styles;
 
 struct _Style_Window
 {
-	Evas_Object *entry_style;
-	Evas_Object *entry_tag;
-	Evas_Object *entry_prop;
+   Evas_Object *entry_style;
+   Evas_Object *entry_tag;
+   Evas_Object *entry_prop;
 };
 
 typedef struct _Style_Window Style_Window;
@@ -61,5 +61,4 @@ typedef struct _Style_Window Style_Window;
 void
 style_viewer_init (App_Data *ap);
 
-#endif	/* UI_STYLE_VIEWER_WINDOW_HEADER_H */
-
+#endif  /* UI_STYLE_VIEWER_WINDOW_HEADER_H */
