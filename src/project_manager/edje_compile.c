2e31cc353748ef59687e5b5e84ab901ea4a6b1a1
rimmed <v.reutskiy@samsung.com>
2013-06-06 18:34:37 +0300

    Update edje compiler

--- src/project_manager/edje_compile.c
+++ src/project_manager/edje_compile.c
@@ -3,17 +3,17 @@
 static void
 compiler_message_clear(Eina_Inlist *list)
 {
-	if(list)
-	{
-		while(list)
-		{
-			Compiler_Message *mes = EINA_INLIST_CONTAINER_GET(list,
-															Compiler_Message);
-			list = eina_inlist_remove(list, EINA_INLIST_GET(mes));
-			free(mes->text);
-			free(mes);
-		}
-	}
+   if(list)
+     {
+        while(list)
+          {
+             Compiler_Message *mes = EINA_INLIST_CONTAINER_GET(list,
+                                                               Compiler_Message);
+             list = eina_inlist_remove(list, EINA_INLIST_GET(mes));
+             free(mes->text);
+             free(mes);
+          }
+     }
 }
 
 /***
@@ -21,125 +21,106 @@ compiler_message_clear(Eina_Inlist *list)
  */
 static Eina_Bool
 exe_data(void *data __UNUSED__,
-			int type __UNUSED__,
-			void *event)
+         int type __UNUSED__,
+         void *event)
 {
-	Ecore_Exe_Event_Data *ev;
-	Eina_Inlist *messages = NULL;
-	Compiler_Message *message = NULL;
+   Ecore_Exe_Event_Data *ev;
+   Eina_Inlist *messages = NULL;
+   Compiler_Message *message = NULL;
 
-	ev = event;
-	messages = (Eina_Inlist *)data;
+   ev = event;
+   messages = (Eina_Inlist *)data;
 
-	if(ev->lines)
-	{
-		int i = 0;
-		for(i = 0; ev->lines[i].line; i++)
-		{
-			message = calloc(1, sizeof(*message));
-			message->time = time(NULL);
-			message->text = strdup(ev->lines[i].line);
-			messages = eina_inlist_append(messages, EINA_INLIST_GET(message));
-			//fprintf(stdout, "%lld\t%s\n", (long long)message->time,
-			//				message->text);
-		}
-	}
+   if(ev->lines)
+     {
+        int i = 0;
+        for (i = 0; ev->lines[i].line; i++)
+          {
+             message = calloc(1, sizeof(*message));
+             message->time = time(NULL);
+             message->text = strdup(ev->lines[i].line);
+             messages = eina_inlist_append(messages, EINA_INLIST_GET(message));
+          }
+     }
 
-	return ECORE_CALLBACK_PASS_ON;
+   ecore_main_loop_quit();
+   return ECORE_CALLBACK_PASS_ON;
 }
 
 Edje_CC *
 compile(const char *edc,
-		const char *edj,
-		const char *image_directory,
-		const char *font_directory,
-		const char *sound_directory)
+        const char *edj,
+        const char *image_directory,
+        const char *font_directory,
+        const char *sound_directory)
 {
-	Edje_CC *edjecc = NULL;
-	Ecore_Exe_Flags flags  = ECORE_EXE_PIPE_READ |
-							ECORE_EXE_PIPE_READ_LINE_BUFFERED |
-							ECORE_EXE_PIPE_ERROR |
-							ECORE_EXE_PIPE_ERROR_LINE_BUFFERED;
-	Compiler_Message *cmes = NULL;
-	char compile_mess[] = "Start compile project: %s";
+   Edje_CC *edjecc = NULL;
+   Ecore_Exe_Flags flags  = ECORE_EXE_PIPE_READ |
+                            ECORE_EXE_PIPE_READ_LINE_BUFFERED |
+                            ECORE_EXE_PIPE_ERROR |
+                            ECORE_EXE_PIPE_ERROR_LINE_BUFFERED;
+   Compiler_Message *cmes = NULL;
+   char compile_mess[] = "Start compile project: %s";
 
-	edjecc = calloc(1, sizeof(*edjecc));
-	sprintf(edjecc->cmd, "edje_cc -id=%s -fd=%s -sd=%s %s %s",
-			image_directory, font_directory, sound_directory,
-			edc, edj);
-	cmes = calloc(1, sizeof(*cmes));
-	cmes->time = time(NULL);
-	cmes->text = malloc(strlen(compile_mess) + strlen(edc) + 32);
-	sprintf(cmes->text, compile_mess, edc);
-	edjecc->messages = eina_inlist_append(NULL, EINA_INLIST_GET(cmes));
+   edjecc = calloc(1, sizeof(*edjecc));
+   sprintf(edjecc->cmd, "edje_cc -id=%s -fd=%s -sd=%s %s %s",
+           image_directory, font_directory, sound_directory,
+           edc, edj);
+   cmes = calloc(1, sizeof(*cmes));
+   cmes->time = time(NULL);
+   cmes->text = malloc(strlen(compile_mess) + strlen(edc) + 32);
+   sprintf(cmes->text, compile_mess, edc);
+   edjecc->messages = eina_inlist_append(NULL, EINA_INLIST_GET(cmes));
 
-	ecore_event_handler_add(ECORE_EXE_EVENT_DATA, exe_data, edjecc->messages);
-	ecore_event_handler_add(ECORE_EXE_EVENT_ERROR, exe_data, edjecc->messages);
+   ecore_event_handler_add(ECORE_EXE_EVENT_DATA, exe_data, edjecc->messages);
+   ecore_event_handler_add(ECORE_EXE_EVENT_ERROR, exe_data, edjecc->messages);
 
-	edjecc->exe = ecore_exe_pipe_run(edjecc->cmd, flags, NULL);
+   edjecc->exe = ecore_exe_pipe_run(edjecc->cmd, flags, NULL);
+   ecore_main_loop_begin();
 
-	return edjecc;
+   return edjecc;
 }
 
 Edje_DeCC *
 decompile(char *edj, char *edc)
 {
-	Edje_DeCC *edjedecc = NULL;
-	Ecore_Exe_Flags flags  = ECORE_EXE_PIPE_READ |
-							ECORE_EXE_PIPE_READ_LINE_BUFFERED |
-							ECORE_EXE_PIPE_ERROR |
-							ECORE_EXE_PIPE_ERROR_LINE_BUFFERED;
-	Compiler_Message *cmes = NULL;
-	char decompile_mess[] = "Start decompile project: %s";
+   Edje_DeCC *edjedecc = NULL;
+   Ecore_Exe_Flags flags  = ECORE_EXE_PIPE_READ |
+      ECORE_EXE_PIPE_READ_LINE_BUFFERED |
+      ECORE_EXE_PIPE_ERROR |
+      ECORE_EXE_PIPE_ERROR_LINE_BUFFERED;
+   Compiler_Message *cmes = NULL;
+   char decompile_mess[] = "Start decompile project: %s";
 
-	edjedecc = calloc(1, sizeof(*edjedecc));
-	if(!edc)
-	{
-		sprintf(edjedecc->cmd, "edje_decc %s -no-build-sh -current-dir", edj);
-	}
-	else
-	{
-		sprintf(edjedecc->cmd, "edje_decc %s -main-out %s -no-build-sh -current-dir",
-				edj, edc);
-	}
-	cmes = calloc(1, sizeof(*cmes));
-	cmes->time = time(NULL);
-	cmes->text = malloc(strlen(decompile_mess) + strlen(edj) + 32);
-	sprintf(cmes->text, decompile_mess, edj);
-	edjedecc->messages = eina_inlist_append(NULL, EINA_INLIST_GET(cmes));
+   edjedecc = calloc(1, sizeof(*edjedecc));
+   if (!edc)
+        sprintf(edjedecc->cmd, "edje_decc %s -no-build-sh -current-dir", edj);
+   else
+        sprintf(edjedecc->cmd,
+                "edje_decc %s -main-out %s -no-build-sh -current-dir",
+                edj, edc);
+   cmes = calloc(1, sizeof(*cmes));
+   cmes->time = time(NULL);
+   cmes->text = malloc(strlen(decompile_mess) + strlen(edj) + 32);
+   sprintf(cmes->text, decompile_mess, edj);
+   edjedecc->messages = eina_inlist_append(NULL, EINA_INLIST_GET(cmes));
 
-	ecore_event_handler_add(ECORE_EXE_EVENT_DATA, exe_data, edjedecc->messages);
-	ecore_event_handler_add(ECORE_EXE_EVENT_ERROR, exe_data, edjedecc->messages);
+   ecore_event_handler_add(ECORE_EXE_EVENT_DATA, exe_data, edjedecc->messages);
+   ecore_event_handler_add(ECORE_EXE_EVENT_ERROR, exe_data, edjedecc->messages);
 
-	edjedecc->exe = ecore_exe_pipe_run(edjedecc->cmd, flags, NULL);
+   edjedecc->exe = ecore_exe_pipe_run(edjedecc->cmd, flags, NULL);
+   ecore_main_loop_begin();
 
-	return edjedecc;
+   return edjedecc;
 }
 
 void
-compiler_free(Edje_CC *compiler)
+edje_cc_free(struct _Edje_CC *edje_cc)
 {
-	if(!compiler)
-	{
-		return;
-	}
+   if(!edje_cc) return;
 
-	ecore_exe_free(compiler->exe);
-	free(compiler->cmd);
-	compiler_message_clear(compiler->messages);
-	free(compiler);
-}
-
-void
-decompiler_free(Edje_DeCC *decompiler)
-{
-	if(!decompiler)
-	{
-		return;
-	}
-
-	ecore_exe_free(decompiler->exe);
-	free(decompiler->cmd);
-	compiler_message_clear(decompiler->messages);
-	free(decompiler);
+   ecore_exe_free(edje_cc->exe);
+   free(edje_cc->cmd);
+   compiler_message_clear(edje_cc->messages);
+   free(edje_cc);
 }
--- src/project_manager/edje_compile.h
+++ src/project_manager/edje_compile.h
@@ -10,59 +10,66 @@
 
 struct _Compiler_Message
 {
-	EINA_INLIST;
-	time_t time;
-	char *text;
+   EINA_INLIST;
+   time_t time;
+   char *text;
 };
 
-/***
+/**
  *
  */
 typedef struct _Compiler_Message Compiler_Message;
 
 struct _Edje_CC
 {
-	Ecore_Exe *exe;
-	char *cmd;
-	Eina_Inlist *messages;
+   Ecore_Exe *exe;
+   char *cmd;
+   Eina_Inlist *messages;
 };
 
-/***
+/**
  *
  */
 typedef struct _Edje_CC Edje_CC;
 
-/***
+/**
  *
  */
 typedef struct _Edje_CC Edje_DeCC;
 
-/***
+/**
  *
  */
 Edje_CC *
 compile(const char *edc,
-		const char *edj,
-		const char *image_directory,
-		const char *font_directory,
-		const char *sound_directory);
+        const char *edj,
+        const char *image_directory,
+        const char *font_directory,
+        const char *sound_directory);
 
-/***
+/**
  *
  */
 Edje_DeCC *
 decompile(char *edj, char *edc);
 
-/***
+/**
  *
  */
-void
-compiler_free(Edje_CC *compiler);
+#define compiler_free(compiler) \
+   edje_cc_free(compiler);
 
-/***
+/**
+ *
+ */
+#define decompiler_free(decompiler) \
+   edje_cc_free(decompiler);
+
+
+/**
  *
  */
 void
-decompiler_free(Edje_DeCC *decompiler);
+edje_cc_free(struct _Edje_CC *edje_cc);
 
 #endif /* EDJE_COMPILE_H */
