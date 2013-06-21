de23eafefafa7a6eab4f00af84b0c24d5654c31e
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2013-06-21 15:38:04 +0300

    Refactoring text_style. now it's not anymore dealing with App_Data. Only Workspace and Project.

---
+++ src/ui/dialogs/style_viewer_dialog.h
@@ -0,0 +1,102 @@
+#ifndef STYLE_VIEWER_DIALOG_HEADER_H
+#define STYLE_VIEWER_DIALOG_HEADER_H
+
+/**
+ * @defgroup TextStyle TextStyle
+ *
+ * This widget emits the following signals:
+ *
+ * @li "ts,res,add": add new text style resource
+ * @li "ts,res,rm": remove text style resource.
+ * @li "ts,res,cp": copy text style resource into new one.
+ * @li "ts,res,modify,tag,add": add tag to the text style resource.
+ * @li "ts,res,modify,tag,rm": remove tag from the resource.
+ * @li "ts,res,modify,tag,change": change tag's data from resource.
+ *
+ * TextStyle is a Inwin that contain information
+ * of the widgets text styles.
+ */
+
+#include <Eina.h>
+#include "efl_tet.h"
+
+enum _Type_Style
+{
+   TAG,
+   TEXTSTYLE
+};
+
+typedef enum _Type_Style Type_Style;
+
+struct _Tag
+{
+   EINA_INLIST;
+   char *tag_name;
+   char *tag_value;
+   Type_Style __type;
+};
+
+typedef struct _Tag Tag;
+
+struct _Text_Styles
+{
+   EINA_INLIST;
+   char *style_name;
+   Eina_Inlist *tags;
+   Type_Style __type;
+};
+
+typedef struct _Text_Styles Text_Styles;
+
+struct _Style_Window
+{
+   Evas_Object *entry_style;
+   Evas_Object *entry_tag;
+   Evas_Object *entry_prop;
+};
+
+typedef struct _Style_Window Style_Window;
+
+/**
+ * Show the whole inwin window by using some data
+ * about loaded project (edj file)
+ *
+ * @param parent - the object that will be the parent of this dialog.
+ * @param ws - working workspace
+ * @param project -currently opened project
+ *
+ * @return the inwin object that contain style viewer
+ *
+ * @ingroup TextStyle
+ */
+Evas_Object *
+style_viewer_add (Evas_Object *parent, Workspace *ws, Project *project);
+
+/**
+ * Set a title for style list
+ *
+ * @param object A style list object
+ * @param title - The title string
+ *
+ * @ingroup TextStyle
+ */
+void
+style_viewer_list_title_set(Evas_Object *object, const char *title);
+
+/**
+ * Add (register) a callback function to a given Style List object event.
+ *
+ * @param object a Style List object
+ * @param event the event's name string
+ * @param func the callback function
+ * @param data user data to be passed to the callback function
+ *
+ * @ingroup TextStyle
+ */
+void
+style_viewer_list_callback_add(Evas_Object *object,
+                            const char *event,
+                            Edje_Signal_Cb func,
+                            void *data);
+
+#endif  /* STYLE_VIEWER_DIALOG_HEADER_H */
