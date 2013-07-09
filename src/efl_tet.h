eb763516c362101f3499fda0b14bed3e33d8b552
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2013-07-09 20:46:48 +0300

    Documentation: efl_tet.c

--- src/efl_tet.h
+++ src/efl_tet.h
@@ -1,3 +1,12 @@
+/**
+ * @defgroup ELM_MAIN ELM_MAIN
+ * @ingroup EFL_TET
+ *
+ * The begging point of the programm
+ * (contain starting function like main and
+ *some functions that free/init base required components)
+ */
+
 #ifndef EFL_TET_H
 #define EFL_TET_H
 
@@ -43,31 +52,55 @@ struct _App_Data
 typedef struct _App_Data App_Data;
 
 /**
+ * This function inits all libraries required by this programm.
+ * Also it will add extension to all edc files required by this programm.
  *
+ * @return EINA_TRUE all libraries initialized succesfully.
+ * EINA_FALSE one of the libraries was failed to inti.
+ *
+ * @ingroup EFL_TET
  */
 Eina_Bool
 app_init(void);
 
 /**
+ * Shutdown of the program. It'll free App_Data, remove extension theme
+ * (custom EDC needed for this program) and shutdown all launched
+ * and inited libraries.
  *
+ * @ingroup EFL_TET
  */
 void
 app_shutdown(void);
 
 /**
+ * This function will return win_layout (if App_Data and win_layout exists).
+ * If they are not exist, then this function will return NULL.
+ * This function is usable for notification.
  *
+ * @return win_layout if exist, NULL if not exist.
+ *
+ * @ingroup EFL_TET
  */
 Evas_Object *
 win_layout_get(void);
 
 /**
+ * Allocate memory for creating App_Data.
  *
+ * @return link to created empty App_Data.
+ *
+ * @ingroup EFL_TET
  */
 App_Data *
 app_create(void);
 
 /**
+ * This function will free allocated memory used for App_Data.
  *
+ * @param ap - App_Data of this program for removing before program shutdown.
+ *
+ * @ingroup EFL_TET
  */
 void
 app_free(App_Data *ap);
