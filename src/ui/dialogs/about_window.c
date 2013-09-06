5867f7df4a071725fccc9edfc226f391dc6b59e4
rimmed <v.reutskiy@samsung.com>
2013-09-06 17:00:12 +0300

    Update about window

--- src/ui/dialogs/about_window.c
+++ src/ui/dialogs/about_window.c
@@ -24,14 +24,15 @@ about_window_add(Evas_Object *parent)
 {
    Evas_Object *mwin = mw_about_add(parent);
    mw_title_set(mwin, "About");
-   const char* labels_text[] = { "Vyacheslav \"rimmed\" Reutskiy (v.reutskiy@samsung.com)",
-                               "Mykyta Biliavskyi (m.biliavskyi@samsung.com)",
-                               "Vitalii Vorobiov (vi.vorobiov@samsung.com)",
-                               "Andrii Kroitor (an.kroitor@samsung.com)",
-                               "",
-                               "Special thanks for the help:",
-                               "Sergey Osadchy (se.osadchy@samsung.com)",
-                               "Kateryna Fesyna (k.fesyna@samsung.com)"};
+   const char* labels_text[] = {"authors:",
+                                "Vyacheslav \"rimmed\" Reutskiy (v.reutskiy@samsung.com)",
+                                "Mykyta Biliavskyi (m.biliavskyi@samsung.com)",
+                                "Vitalii Vorobiov (vi.vorobiov@samsung.com)",
+                                "Andrii Kroitor (an.kroitor@samsung.com)",
+                                "",
+                                "Special thanks for the help:",
+                                "Sergey Osadchy (se.osadchy@samsung.com)",
+                                "Kateryna Fesyna (k.fesyna@samsung.com)"};
    Evas_Object *label, *box;
    int i;
 
@@ -42,7 +43,7 @@ about_window_add(Evas_Object *parent)
    evas_object_show(box);
 
    label = elm_label_add(mwin);
-   elm_object_text_set(label, "Theme-Editor-Tool authors:");
+   elm_object_text_set(label, "Theme-Editor-Tool v."VERSION);
    elm_object_style_set(label, "marker");
    evas_object_color_set(label, 0, 0, 0, 255);
    elm_box_pack_end(box, label);
