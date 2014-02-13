34a4a70ef3b3f7385222bcab858e8f4a6a4a2b2c
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2014-02-13 16:34:14 +0200

    UTC: Widget Manager/wm_widget_list_free

--- tests/test_widget_manager/test_widget_manager.c
+++ tests/test_widget_manager/test_widget_manager.c
@@ -59,8 +59,8 @@ Suite* test_suite (void) {
 /*   tcase_add_test(tcase, wm_widget_free_test_n); */
 /*   tcase_add_test(tcase, wm_widget_list_new_test_p); */
 /*   tcase_add_test(tcase, wm_widget_list_new_test_n); */
-/*   tcase_add_test(tcase, wm_widget_list_free_test_p); */
-/*   tcase_add_test(tcase, wm_widget_list_free_test_n); */
+   tcase_add_test(tcase, wm_widget_list_free_test_p);
+   tcase_add_test(tcase, wm_widget_list_free_test_n);
 /*   tcase_add_test(tcase, wm_style_object_find_test_p1); */
 /*   tcase_add_test(tcase, wm_style_object_find_test_p2); */
 /*   tcase_add_test(tcase, wm_style_object_find_test_n1); */
