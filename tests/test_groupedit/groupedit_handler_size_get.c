ca2a8316f6ccab29013ab6e8329f9b4efbd9ce55
rimmed <v.reutskiy@samsung.com>
2014-02-14 10:30:32 +0200

    UTC: groupedit/groupedit_handler_size_get, groupedit_handler_size_set

--- tests/test_groupedit/groupedit_handler_size_get.c
+++ tests/test_groupedit/groupedit_handler_size_get.c
@@ -32,6 +32,87 @@
 /**
  * @addtogroup groupedit_handler_size_get
  * @{
+ * <tr>
+ * <td>groupedit_handler_size_get</td>
+ * <td>groupedit_handler_size_get_test_p</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ * @step 2 create parent window
+ * @step 3 create a groupedit
+ *
+ * @procedure
+ * @step 1 get the new size for groupedit handlers
+ * @step 2 check size of groupedit handlers
+ * </td>
+ * <td>(Evas_Object *) groupedit</td>
+ * <td>5, 5, 5, 5</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_get_test_p)
+{
+   Evas_Object *parent, *groupedit;
+   int lt_w, lt_h, br_w, br_h;
+
+   elm_init(0, 0);
+   lt_w = lt_h = br_w = br_h = 0;
+
+   parent = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+
+   groupedit = groupedit_add(parent);
+   groupedit_handler_size_get(groupedit, &lt_w, &lt_h, &br_w, &br_h);
+   ck_assert_msg(((lt_w == 5) || (lt_h == 5) || (br_w == 5) || (br_h == 5)),
+                 "Failed to set the groupedit handlers size.");
+
+   evas_object_del(parent);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_get
+ * @{
+ * <tr>
+ * <td>groupedit_handler_size_get</td>
+ * <td>groupedit_handler_size_get_test_n</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ * @step 2 create parent window
+ *
+ * @procedure
+ * @step 1 get the new size for groupedit handlers
+ * @step 2 check size of groupedit handlers
+ * </td>
+ * <td>NULL</td>
+ * <td>0, 0, 0, 0</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_get_test_n)
+{
+   int lt_w, lt_h, br_w, br_h;
+
+   elm_init(0, 0);
+   lt_w = lt_h = br_w = br_h = 0;
+
+   groupedit_handler_size_get(NULL, &lt_w, &lt_h, &br_w, &br_h);
+   ck_assert_msg(((lt_w == 0) || (lt_h == 0) || (br_w == 0) || (br_h == 0)),
+                 "Failed to set the groupedit handlers size.");
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_get
+ * @{
  * </TABLE>
  * @}
  * @}
--- tests/test_groupedit/groupedit_handler_size_set.c
+++ tests/test_groupedit/groupedit_handler_size_set.c
@@ -32,6 +32,267 @@
 /**
  * @addtogroup groupedit_handler_size_set
  * @{
+ * <tr>
+ * <td>groupedit_handler_size_set</td>
+ * <td>groupedit_handler_size_set_test_p</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ * @step 2 create parent window
+ * @step 3 create a groupedit
+ *
+ * @procedure
+ * @step 1 set the new size for groupedit handlers
+ * @step 2 check returned value
+ * </td>
+ * <td>(Evas_Object *) groupedit, 9, 9, 9, 9</td>
+ * <td>EINA_TRUE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_set_test_p)
+{
+   Evas_Object *parent, *groupedit;
+
+   elm_init(0, 0);
+
+   parent = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+
+   groupedit = groupedit_add(parent);
+   ck_assert_msg(groupedit_handler_size_set(groupedit, 9, 9, 9, 9),
+                 "Failed to set the groupedit handlers size.");
+
+   evas_object_del(parent);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_set
+ * @{
+ * <tr>
+ * <td>groupedit_handler_size_set</td>
+ * <td>groupedit_handler_size_set_test_n1</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ *
+ * @procedure
+ * @step 1 set the new size for groupedit handlers
+ * @step 2 check returned value
+ * </td>
+ * <td>NULL, 9, 9, 9, 9</td>
+ * <td>EINA_FALSE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_set_test_n1)
+{
+   elm_init(0, 0);
+
+   ck_assert_msg(!groupedit_handler_size_set(NULL, 9, 9, 9, 9),
+                 "Failed to set the groupedit handlers size.");
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_set
+ * @{
+ * <tr>
+ * <td>groupedit_handler_size_set</td>
+ * <td>groupedit_handler_size_set_test_n2</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ * @step 2 create parent window
+ * @step 3 create a groupedit
+ *
+ * @procedure
+ * @step 1 set the new size for groupedit handlers
+ * @step 2 check returned value
+ * </td>
+ * <td>(Evas_Object *)groupedit, 0, 9, 9, 9</td>
+ * <td>EINA_TRUE (5, 9, 9, 9)</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_set_test_n2)
+{
+   Evas_Object *parent, *groupedit;
+   int lt_w, lt_h, br_w, br_h;
+   Eina_Bool res;
+
+   elm_init(0, 0);
+   lt_w = lt_h = br_w = br_h = 0;
+
+   parent = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+
+   groupedit = groupedit_add(parent);
+   res = groupedit_handler_size_set(groupedit, 0, 9, 9, 9);
+   if (res)
+     groupedit_handler_size_get(groupedit, &lt_w, &lt_h, &br_w, &br_h);
+   ck_assert_msg(((!res) || (lt_w == 5) || (lt_h == 9) || (br_w == 9) || (br_h == 9)),
+                 "Failed to set the groupedit handlers size.");
+
+   evas_object_del(parent);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_set
+ * @{
+ * <tr>
+ * <td>groupedit_handler_size_set</td>
+ * <td>groupedit_handler_size_set_test_n3</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ * @step 2 create parent window
+ * @step 3 create a groupedit
+ *
+ * @procedure
+ * @step 1 set the new size for groupedit handlers
+ * @step 2 check returned value
+ * </td>
+ * <td>(Evas_Object *)groupedit, 9, 0, 9, 9</td>
+ * <td>EINA_TRUE (9, 5, 9, 9)</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_set_test_n3)
+{
+   Evas_Object *parent, *groupedit;
+   int lt_w, lt_h, br_w, br_h;
+   Eina_Bool res;
+
+   elm_init(0, 0);
+   lt_w = lt_h = br_w = br_h = 0;
+
+   parent = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+
+   groupedit = groupedit_add(parent);
+   res = groupedit_handler_size_set(groupedit, 9, 0, 9, 9);
+   if (res)
+     groupedit_handler_size_get(groupedit, &lt_w, &lt_h, &br_w, &br_h);
+   ck_assert_msg(((!res) || (lt_w == 9) || (lt_h == 5) || (br_w == 9) || (br_h == 9)),
+                 "Failed to set the groupedit handlers size.");
+
+   evas_object_del(parent);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_set
+ * @{
+ * <tr>
+ * <td>groupedit_handler_size_set</td>
+ * <td>groupedit_handler_size_set_test_n4</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ * @step 2 create parent window
+ * @step 3 create a groupedit
+ *
+ * @procedure
+ * @step 1 set the new size for groupedit handlers
+ * @step 2 check return value
+ * </td>
+ * <td>(Evas_Object *)groupedit, 9, 9, 0, 9</td>
+ * <td>EINA_TRUE (9, 9, 5, 9)</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_set_test_n4)
+{
+   Evas_Object *parent, *groupedit;
+   int lt_w, lt_h, br_w, br_h;
+   Eina_Bool res;
+
+   elm_init(0, 0);
+   lt_w = lt_h = br_w = br_h = 0;
+
+   parent = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+
+   groupedit = groupedit_add(parent);
+   res = groupedit_handler_size_set(groupedit, 9, 9, 0, 9);
+   if (res)
+     groupedit_handler_size_get(groupedit, &lt_w, &lt_h, &br_w, &br_h);
+   ck_assert_msg(((!res) || (lt_w == 9) || (lt_h == 9) || (br_w == 5) || (br_h == 9)),
+                 "Failed to set the groupedit handlers size.");
+
+   evas_object_del(parent);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_set
+ * @{
+ * <tr>
+ * <td>groupedit_handler_size_set</td>
+ * <td>groupedit_handler_size_set_test_n5</td>
+ * <td>
+ * @precondition
+ * @step 1 initialize elementary library
+ * @step 2 create parent window
+ * @step 3 create a groupedit
+ *
+ * @procedure
+ * @step 1 set the new size for groupedit handlers
+ * @step 2 check returned value
+ * </td>
+ * <td>(Evas_Object *)groupedit, 9, 9, 9, 0</td>
+ * <td>EINA_TRUE (9, 9, 9, 5)</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(groupedit_handler_size_set_test_n5)
+{
+   Evas_Object *parent, *groupedit;
+   int lt_w, lt_h, br_w, br_h;
+   Eina_Bool res;
+
+   elm_init(0, 0);
+   lt_w = lt_h = br_w = br_h = 0;
+
+   parent = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+
+   groupedit = groupedit_add(parent);
+   res = groupedit_handler_size_set(groupedit, 9, 9, 9, 0);
+   if (res)
+     groupedit_handler_size_get(groupedit, &lt_w, &lt_h, &br_w, &br_h);
+   ck_assert_msg(((!res) || (lt_w == 9) || (lt_h == 9) || (br_w == 9) || (br_h == 5)),
+                 "Failed to set the groupedit handlers size.");
+
+   evas_object_del(parent);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup groupedit_handler_size_set
+ * @{
  * </TABLE>
  * @}
  * @}
