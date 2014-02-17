adc7510e5cf37cdbf0d92840d2d85aec8f4ab0d2
Andrii Kroitor <an.kroitor@samsung.com>
2014-02-17 16:55:04 +0200

    UTC: style_editor/style_editor_window_add

---
+++ tests/test_style_editor/test_style_editor.h
@@ -0,0 +1,31 @@
+/* Edje Theme Editor
+* Copyright (C) 2013 Samsung Electronics.
+*
+* This file is part of Edje Theme Editor.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; If not, see .
+*/
+
+#include "utc_common.h"
+#include "style_editor.h"
+#include "ui_main_window.h"
+
+/**
+ * @defgroup style_editor_test
+ */
+
+void style_editor_window_add_test_p(int);
+void style_editor_window_add_test_n1(int);
+void style_editor_window_add_test_n2(int);
+void style_editor_window_add_test_n3(int);
