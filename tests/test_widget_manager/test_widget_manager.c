086dbaff8d0c8c7b8c73c49971285132e8c3e9a0
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2014-02-26 12:41:12 +0200

    UTC: Widget Manager/wm_style_object_find

--- tests/test_widget_manager/test_widget_manager.c
+++ tests/test_widget_manager/test_widget_manager.c
@@ -61,11 +61,11 @@ Suite* test_suite (void) {
    tcase_add_test(tcase, wm_widget_list_free_test_n);
    tcase_add_test(tcase, wm_widget_list_new_test_p);
    tcase_add_test(tcase, wm_widget_list_new_test_n);
-/*   tcase_add_test(tcase, wm_style_object_find_test_p1); */
-/*   tcase_add_test(tcase, wm_style_object_find_test_p2); */
-/*   tcase_add_test(tcase, wm_style_object_find_test_n1); */
-/*   tcase_add_test(tcase, wm_style_object_find_test_n2); */
-/*   tcase_add_test(tcase, wm_style_object_find_test_n3); */
+   tcase_add_test(tcase, wm_style_object_find_test_p1);
+   tcase_add_test(tcase, wm_style_object_find_test_p2);
+   tcase_add_test(tcase, wm_style_object_find_test_n1);
+   tcase_add_test(tcase, wm_style_object_find_test_n2);
+   tcase_add_test(tcase, wm_style_object_find_test_n3);
    tcase_add_test(tcase, wm_part_type_get_test_p1);
    tcase_add_test(tcase, wm_part_type_get_test_p2);
    tcase_add_test(tcase, wm_part_type_get_test_n1);
