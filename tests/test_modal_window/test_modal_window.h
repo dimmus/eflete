e29701b3d3a60cbbe514470af18191b00d090527
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2014-03-13 17:45:27 +0200

    UTC: modal_window/INITIAL_STRUCTURE_COMMIT

---
+++ tests/test_modal_window/test_modal_window.h
@@ -0,0 +1,40 @@
+/* Edje Theme Editor
+* Copyright (C) 2014 Samsung Electronics.
+*
+* This file is part of Edje Theme Editor.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; If not, see .
+*/
+
+#include "utc_common.h"
+#include "modal_window.h"
+#include "ui_main_window.h"
+
+/**
+ * @defgroup modal_window_test
+ */
+
+void  mw_add_test_p(int);
+void  mw_add_test_n(int);
+void  mw_about_add_test_p(int);
+void  mw_about_add_test_n(int);
+void  mw_title_set_test_n1(int);
+void  mw_title_set_test_p(int);
+void  mw_title_set_test_n2(int);
+void  mw_info_text_set_test_p(int);
+void  mw_info_text_set_test_n1(int);
+void  mw_info_text_set_test_n2(int);
+void  mw_icon_set_test_p(int);
+void  mw_icon_set_test_n1(int);
+void  mw_icon_set_test_n2(int);
