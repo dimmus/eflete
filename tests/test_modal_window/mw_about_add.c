fd394aa549dad4d7a07bfbb6c14de241a44309b5
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2014-03-18 14:42:49 +0200

    UTC: modal_window/mw_about_add
    Adding two tests, negative and positive,
    that test the work of mw_about_add function
    that add a modal_window (about style) using main_window object.

--- tests/test_modal_window/mw_about_add.c
+++ tests/test_modal_window/mw_about_add.c
@@ -29,6 +29,75 @@
  * @}
  */
 
+/**
+ * @addtogroup mw_about_add
+ * @{
+ * <tr>
+ * <td>mw_about_add</td>
+ * <td>mw_about_add_test_p</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized efl and app
+ * @step 2 main_window created
+ *
+ * @procedure
+ * @step 1 Call mw_about_add(NULL, NULL)
+ * @step 2 Check returned pointer
+ * </td>
+ * <td>NULL, NULL</td>
+ * <td>Not NULL pointer returned</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST (mw_about_add_test_p)
+{
+   elm_init(0,0);
+   App_Data *app;
+
+   app_init();
+   app = app_create();
+   ui_main_window_add(app);
+   ck_assert_msg(mw_about_add(NULL, NULL) != NULL, "cannot create new Modal Window");
+
+   app_shutdown();
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup mw_about_add
+ * @{
+ * <tr>
+ * <td>mw_about_add</td>
+ * <td>mw_about_add_test_n</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized efl and app
+ * @step 2 main_window NOT created
+ *
+ * @procedure
+ * @step 1 Call mw_about_add(NULL, NULL)
+ * @step 2 Check returned pointer
+ * </td>
+ * <td>NULL, NULL</td>
+ * <td>NULL pointer returned</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST (mw_about_add_test_n)
+{
+   elm_init(0,0);
+
+   app_init();
+   ck_assert_msg(mw_about_add(NULL, NULL) == NULL, "Not NULL pointer returned");
+
+   elm_shutdown();
+}
+END_TEST
 
 /**
  * @addtogroup mw_about_add
