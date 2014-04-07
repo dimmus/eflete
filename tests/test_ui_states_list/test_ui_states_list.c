0501d5d56ab33e03aedf3728a283bfe5117baf04
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2014-04-07 19:21:38 +0300

    UTC: ui_states_list/ui_states_list_data_set

--- tests/test_ui_states_list/test_ui_states_list.c
+++ tests/test_ui_states_list/test_ui_states_list.c
@@ -24,12 +24,12 @@ Suite* test_suite (void) {
    TCase *tcase = tcase_create("TCase");
    tcase_add_test(tcase, ui_states_list_add_test_p);
    tcase_add_test(tcase, ui_states_list_add_test_n);
-/*tcase_add_test(tcase, ui_states_list_data_set_test_p); */
-/*tcase_add_test(tcase, ui_states_list_data_set_test_n1); */
-/*tcase_add_test(tcase, ui_states_list_data_set_test_n2); */
-/*   tcase_add_test(tcase, ui_states_list_data_set_test_n3); */
- /*  tcase_add_test(tcase, ui_states_list_data_set_test_n4); */
- /*  tcase_add_test(tcase, ui_states_list_data_set_test_n5); */
+   tcase_add_test(tcase, ui_states_list_data_set_test_p);
+   tcase_add_test(tcase, ui_states_list_data_set_test_n1);
+   tcase_add_test(tcase, ui_states_list_data_set_test_n2);
+   tcase_add_test(tcase, ui_states_list_data_set_test_n3);
+   tcase_add_test(tcase, ui_states_list_data_set_test_n4);
+   tcase_add_test(tcase, ui_states_list_data_set_test_n5);
  /*  tcase_add_test(tcase, ui_states_list_data_set_test_n6); */
  /*  tcase_add_test(tcase, ui_states_list_part_get_test_p1); */
  /*  tcase_add_test(tcase, ui_states_list_part_get_test_p2); */
--- tests/test_ui_states_list/ui_states_list_data_set.c
+++ tests/test_ui_states_list/ui_states_list_data_set.c
@@ -33,6 +33,352 @@
 /**
  * @addtogroup ui_states_list_data_set
  * @{
+ * <tr>
+ * <td>ui_states_list_data_set</td>
+ * <td>ui_states_list_data_set_test_p</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized elm.
+ * @step 2 add theme extension "eflete theme".
+ * @step 3 created Window.
+ * @step 4 Style filled with data.
+ * @step 5 created States List
+ * @step 6 Part which states will be shown and set.
+ *
+ * @procedure
+ * @step 1 Call function ui_states_list_data_set(gl_states, style, part).
+ * @step 2 Check returned value.
+ * </td>
+ * <td>(Evas_Object *) gl_states, (Style *) style, (Part *) part</td>
+ * <td>EINA_TRUE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(ui_states_list_data_set_test_p)
+{
+   Evas_Object *window, *gl_states;
+   Evas *e;
+   Style *style;
+   Part *part;
+   const char *edj = "./edj_build/radio.edj";
+   const char *style_name = "def";
+   const char *full_style_name = "elm/radio/base/def";
+
+   elm_init(0,0);
+   elm_theme_extension_add(NULL, EFLETE_THEME);
+   window = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+   e = evas_object_evas_get(window);
+   style = wm_style_add(style_name, full_style_name, STYLE);
+   wm_style_data_load(style, e, edj);
+   gl_states = ui_states_list_add(window);
+   part = EINA_INLIST_CONTAINER_GET(style->parts, Part);
+
+   ck_assert_msg(ui_states_list_data_set(gl_states, style, part) == EINA_TRUE,
+                 "Data not setted to the State List");
+
+   elm_theme_extension_del(NULL, EFLETE_THEME);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup ui_states_list_data_set
+ * @{
+ * <tr>
+ * <td>ui_states_list_data_set</td>
+ * <td>ui_states_list_data_set_test_n1</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized elm.
+ * @step 2 add theme extension "eflete theme".
+ * @step 3 created Window.
+ * @step 4 Style filled with data.
+ * @step 5 created States List
+ *
+ * @procedure
+ * @step 1 Call function ui_states_list_data_set(gl_states, style, NULL).
+ * @step 2 Check returned value.
+ * </td>
+ * <td>(Evas_Object *) gl_states, (Style *) style, NULL</td>
+ * <td>EINA_FALSE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(ui_states_list_data_set_test_n1)
+{
+   Evas_Object *window, *gl_states;
+   Evas *e;
+   Style *style;
+   const char *edj = "./edj_build/radio.edj";
+   const char *style_name = "def";
+   const char *full_style_name = "elm/radio/base/def";
+
+   elm_init(0,0);
+   elm_theme_extension_add(NULL, EFLETE_THEME);
+   window = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+   e = evas_object_evas_get(window);
+   style = wm_style_add(style_name, full_style_name, STYLE);
+   wm_style_data_load(style, e, edj);
+   gl_states = ui_states_list_add(window);
+
+   ck_assert_msg(ui_states_list_data_set(gl_states, style, NULL) == EINA_FALSE,
+                 "Data was set to the State List");
+
+   elm_theme_extension_del(NULL, EFLETE_THEME);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup ui_states_list_data_set
+ * @{
+ * <tr>
+ * <td>ui_states_list_data_set</td>
+ * <td>ui_states_list_data_set_test_n2</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized elm.
+ * @step 2 add theme extension "eflete theme".
+ * @step 3 created Window.
+ * @step 4 Style filled with data.
+ * @step 5 created States List
+ * @step 6 Part which states will be shown and set.
+ *
+ * @procedure
+ * @step 1 Call function ui_states_list_data_set(gl_states, NULL, part).
+ * @step 2 Check returned value.
+ * </td>
+ * <td>(Evas_Object *) gl_states, NULL, (Part *) part</td>
+ * <td>EINA_FALSE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(ui_states_list_data_set_test_n2)
+{
+   Evas_Object *window, *gl_states;
+   Evas *e;
+   Style *style;
+   Part *part;
+   const char *edj = "./edj_build/radio.edj";
+   const char *style_name = "def";
+   const char *full_style_name = "elm/radio/base/def";
+
+   elm_init(0,0);
+   elm_theme_extension_add(NULL, EFLETE_THEME);
+   window = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+   e = evas_object_evas_get(window);
+   style = wm_style_add(style_name, full_style_name, STYLE);
+   wm_style_data_load(style, e, edj);
+   gl_states = ui_states_list_add(window);
+   part = EINA_INLIST_CONTAINER_GET(style->parts, Part);
+
+   ck_assert_msg(ui_states_list_data_set(gl_states, NULL, part) == EINA_FALSE,
+                 "Data was set to the State List");
+
+   elm_theme_extension_del(NULL, EFLETE_THEME);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup ui_states_list_data_set
+ * @{
+ * <tr>
+ * <td>ui_states_list_data_set</td>
+ * <td>ui_states_list_data_set_test_n3</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized elm.
+ * @step 2 add theme extension "eflete theme".
+ * @step 3 created Window.
+ * @step 4 Style filled with data.
+ * @step 5 Part which states will be shown and set.
+ *
+ * @procedure
+ * @step 1 Call function ui_states_list_data_set(NULL, style, part).
+ * @step 2 Check returned value.
+ * </td>
+ * <td>NULL, (Style *) style, (Part *) part</td>
+ * <td>EINA_FALSE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(ui_states_list_data_set_test_n3)
+{
+   Evas_Object *window;
+   Evas *e;
+   Style *style;
+   Part *part;
+   const char *edj = "./edj_build/radio.edj";
+   const char *style_name = "def";
+   const char *full_style_name = "elm/radio/base/def";
+
+   elm_init(0,0);
+   elm_theme_extension_add(NULL, EFLETE_THEME);
+   window = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+   e = evas_object_evas_get(window);
+   style = wm_style_add(style_name, full_style_name, STYLE);
+   wm_style_data_load(style, e, edj);
+   part = EINA_INLIST_CONTAINER_GET(style->parts, Part);
+
+   ck_assert_msg(ui_states_list_data_set(NULL, style, part) == EINA_FALSE,
+                 "Data was set to the State List");
+
+   elm_theme_extension_del(NULL, EFLETE_THEME);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup ui_states_list_data_set
+ * @{
+ * <tr>
+ * <td>ui_states_list_data_set</td>
+ * <td>ui_states_list_data_set_test_n4</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized elm.
+ *
+ * @procedure
+ * @step 1 Call function ui_states_list_data_set(gl_states, style, part).
+ * @step 2 Check returned value.
+ * </td>
+ * <td>NULL, NULL, NULL</td>
+ * <td>EINA_FALSE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(ui_states_list_data_set_test_n4)
+{
+   elm_init(0,0);
+
+   ck_assert_msg(ui_states_list_data_set(NULL, NULL, NULL) == EINA_FALSE,
+                 "Data was set to the State List");
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup ui_states_list_data_set
+ * @{
+ * <tr>
+ * <td>ui_states_list_data_set</td>
+ * <td>ui_states_list_data_set_test_n5</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized elm.
+ * @step 2 add theme extension "eflete theme".
+ * @step 3 created Window.
+ * @step 4 Style filled with data.
+ * @step 5 Part which states will be shown and set.
+ *
+ * @procedure
+ * @step 1 Call function ui_states_list_data_set(window, style, part).
+ * @step 2 Check returned value.
+ * </td>
+ * <td>(Evas_Object *) window, (Style *) style, (Part *) part</td>
+ * <td>EINA_FALSE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(ui_states_list_data_set_test_n5)
+{
+   Evas_Object *window;
+   Evas *e;
+   Style *style;
+   Part *part;
+   const char *edj = "./edj_build/radio.edj";
+   const char *style_name = "def";
+   const char *full_style_name = "elm/radio/base/def";
+
+   elm_init(0,0);
+   elm_theme_extension_add(NULL, EFLETE_THEME);
+   window = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+   e = evas_object_evas_get(window);
+   style = wm_style_add(style_name, full_style_name, STYLE);
+   wm_style_data_load(style, e, edj);
+   part = EINA_INLIST_CONTAINER_GET(style->parts, Part);
+
+   ck_assert_msg(ui_states_list_data_set(window, style, part) == EINA_FALSE,
+                 "Data was set to the State List");
+
+   elm_theme_extension_del(NULL, EFLETE_THEME);
+
+   elm_shutdown();
+}
+END_TEST
+
+/**
+ * @addtogroup ui_states_list_data_set
+ * @{
+ * <tr>
+ * <td>ui_states_list_data_set</td>
+ * <td>ui_states_list_data_set_test_n6</td>
+ * <td>
+ * @precondition
+ * @step 1 initialized elm.
+ * @step 2 add theme extension "eflete theme".
+ * @step 3 created Window.
+ * @step 4 Empty Style.
+ * @step 5 created States List
+ * @step 6 Part which states will be shown and set.
+ *
+ * @procedure
+ * @step 1 Call function ui_states_list_data_set(gl_states, style, part).
+ * @step 2 Check returned value.
+ * </td>
+ * <td>(Evas_Object *) gl_states, (Style *) style, (Part *) part</td>
+ * <td>EINA_TRUE</td>
+ * <td>_REAL_RESULT_</td>
+ * <td>_PASSED_</td>
+ * </tr>
+ * @}
+ */
+EFLETE_TEST(ui_states_list_data_set_test_n6)
+{
+   Evas_Object *window, *gl_states;
+   Style *style;
+   Part *part;
+   const char *style_name = "def";
+   const char *full_style_name = "elm/radio/base/def";
+
+   elm_init(0,0);
+   elm_theme_extension_add(NULL, EFLETE_THEME);
+   window = elm_win_add(NULL, "test", ELM_WIN_BASIC);
+   style = wm_style_add(style_name, full_style_name, STYLE);
+   gl_states = ui_states_list_add(window);
+   part = EINA_INLIST_CONTAINER_GET(style->parts, Part);
+
+   ck_assert_msg(ui_states_list_data_set(gl_states, style, part) == EINA_FALSE,
+                 "Data was setted to the State List with empty Style");
+
+   elm_theme_extension_del(NULL, EFLETE_THEME);
+
+   elm_shutdown();
+}
+END_TEST
+
+
+/**
+ * @addtogroup ui_states_list_data_set
+ * @{
  * </TABLE>
  * @}
  * @}
