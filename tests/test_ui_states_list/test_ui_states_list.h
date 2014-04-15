3c6fba077c6ace90bc1cfe5e16e9b1a7556af424
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2014-04-15 15:57:54 +0300

    UTC: ui_states_list/some fixes.
    1. fixing report generation.
    2. deleting tests for deleted ui_states_list function.

--- tests/test_ui_states_list/test_ui_states_list.h
+++ tests/test_ui_states_list/test_ui_states_list.h
@@ -1,21 +1,22 @@
-/* Edje Theme Editor
-* Copyright (C) 2014 Samsung Electronics.
-*
-* This file is part of Edje Theme Editor.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2, or (at your option)
-* any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; If not, see www.gnu.org/licenses/gpl-2.0.html.
-*/
+/**
+ * Edje Theme Editor
+ * Copyright (C) 2013-2014 Samsung Electronics.
+ *
+ * This file is part of Edje Theme Editor.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see www.gnu.org/licenses/gpl-2.0.html.
+ */
 
 #include "utc_common.h"
 #include "ui_states_list.h"
@@ -45,9 +46,6 @@ void ui_states_list_state_add_test_n3(int);
 void ui_states_list_selected_state_get_test_p(int);
 void ui_states_list_selected_state_get_test_n1(int);
 void ui_states_list_selected_state_get_test_n2(int);
-void ui_states_list_selected_states_get_test_p(int);
-void ui_states_list_selected_states_get_test_n1(int);
-void ui_states_list_selected_states_get_test_n2(int);
 void ui_states_list_selected_states_del_test_p(int);
 void ui_states_list_selected_states_del_test_n1(int);
 void ui_states_list_selected_states_del_test_n2(int);
