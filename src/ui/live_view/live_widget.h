9f9ce3a87f44bf3faca05b825034140723d1c066
Vorobiov Vitalii <vi.vorobiov@samsung.com>
2014-07-21 14:20:39 +0300

    Live View Refactoring: adding documentation for Live_Widget module.

--- src/ui/live_view/live_widget.h
+++ src/ui/live_view/live_widget.h
@@ -32,9 +32,39 @@
 #include <Evas.h>
 #include <Elementary.h>
 
+/**
+ * Create widget according to it's name, class and style.
+ *
+ * This function will create widget/widgets for showing loaded group in Live View.
+ * Created widget will contain callback functions as a data in it, so they are able
+ * to set up in Live View Propery for setting text and swallows of that widget.
+ *
+ * @param widget name of requested widget.
+ * @param class name of class of requested widget.
+ * @param style name of style of current widget.
+ * @param parent Parent object of created widget.
+ *
+ * @return Evas_Object of created widget.
+ *
+ * @ingroup Live_View_Widget
+ */
 Evas_Object *
 live_widget_create(const char *widget, const char *class, const char *style, Evas_Object *parent);
 
+/**
+ * Delete previously created widget object.
+ *
+ * This function delete created widget object and also delete all data (for example
+ * callback functions) that are exists in given widget object.
+ * if there is no widgets inside of that widget, then it probably wrong object and
+ * function will return EINA_FALSE.
+ *
+ * @param widget created widget object for deleting.
+ *
+ * @return EINA_TRUE if successful, EINA_FALSE otherwise;
+ *
+ * @ingroup Live_View_Widget
+ */
 Eina_Bool
 live_widget_del(Evas_Object *widget);
 
