3f2d1ae721b6982b67e1a4a73a8a1089fd612df0
Andrii Kroitor <an.kroitor@samsung.com>
2014-09-16 11:27:43 +0300

    main_window: reenabling menu after closing "not saved" popup
    @fix

--- src/ui/connector.c
+++ src/ui/connector.c
@@ -811,6 +811,7 @@ ui_close_project_request(App_Data *ap, const char *msg)
 
    ecore_main_loop_begin();
 
+   ui_menu_locked_set(ap->menu_hash, false);
    ap->project->close_request = false;
    evas_object_del(ap->popup);
 
