d12ac8fd4de2ce1c3d4cb4606a3ff9bfd0152034
Maksym Volodin <m.volodin@samsung.com>
2014-10-24 17:44:52 +0300

    Preferences: add layout for general form

--- src/ui/preference/preference.c
+++ src/ui/preference/preference.c
@@ -21,11 +21,18 @@
 #include "main_window.h"
 
 typedef struct _Preferences Preferences;
+static Elm_Genlist_Item_Class *_itc = NULL;
 
 struct _Preferences
 {
    Project *pr;
    Evas_Object *mwin;
+   Evas_Object *glist;
+   struct
+   {
+      Evas_Object *general;
+      Evas_Object *workspace;
+   } layout;
 };
 
 static void
@@ -38,16 +45,6 @@ _on_edit_preferences_exit(void *data,
 }
 
 static void
-_on_preferences_close(void *data,
-                      Evas *e __UNUSED__,
-                      Evas_Object *obj __UNUSED__,
-                      void *event_info __UNUSED__)
-{
-   Preferences *preference = (Preferences *)data;
-   free(preference);
-}
-
-static void
 _on_mwin_del(void * data,
              Evas *e __UNUSED__,
              Evas_Object *obj __UNUSED__,
@@ -57,10 +54,166 @@ _on_mwin_del(void * data,
    ui_menu_locked_set(ap->menu_hash, false);
 }
 
+static void
+_on_preferences_close(void *data,
+                      Evas *e __UNUSED__,
+                      Evas_Object *obj __UNUSED__,
+                      void *event_info __UNUSED__)
+{
+   Preferences *preference = (Preferences *)data;
+   free(preference);
+}
+
+/* For GenList, getting the content for showing. Tag Names. */
+static char *
+_item_label_get(void *data,
+                Evas_Object *obj __UNUSED__,
+                const char *part __UNUSED__)
+{
+   char *label = (char *)data;
+   if (!label)
+     {
+        ERR("It impossible, but it is occurred, tag's name is missing!");
+        return NULL;
+     }
+   return strdup(label);
+}
+
+#define pref_layout preference->layout
+#define prof_general pr->general
+
+/* TODO: add field name into Profile struct  */
+
+static void
+_general_form(Preferences *preference)
+{
+   Evas_Object *panes, *layout, *profile;
+   Evas_Object *entry_home, *entry_swap;
+   Profile *pr;
+   /*
+    * Eina_List *prof_list, *l;
+    * const char *version;
+    * prof_list = profiles_get();
+    */
+   pr = profile_get();
+   panes = evas_object_data_get(preference->mwin, "panes");
+   layout = elm_object_part_content_get(panes, "right");
+   if (layout)
+     evas_object_hide(layout);
+   if (!pref_layout.general)
+     {
+        pref_layout.general = elm_layout_add(preference->mwin);
+        evas_object_size_hint_weight_set(pref_layout.general, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+        elm_layout_file_set(pref_layout.general, EFLETE_EDJ, "elm/layout/preferences/general");
+        elm_object_part_content_set(panes, "right", pref_layout.general);
+     }
+   evas_object_show(pref_layout.general);
+
+   EWE_COMBOBOX_ADD(pref_layout.general, profile);
+   elm_object_part_content_set(pref_layout.general, "swallow.profile", profile);
+   ewe_combobox_text_set(profile, _("Loaded profile"));
+   elm_object_disabled_set(profile, true);
+   /*
+    * evas_object_smart_callback_add(profile, "selected", _on_##VALUE##_change, style_edit);
+    * EINA_LIST_FOREACH(prof_list, l, pr)
+    *    {
+    *       version = eina_stringshare_printf("%d", pr->version);
+    *       ewe_combobox_item_add(profile, version);
+    *    }
+    */
+   EWE_ENTRY_ADD(pref_layout.general, entry_home, true, DEFAULT_STYLE);
+   elm_object_part_text_set(entry_home, "elm.text", prof_general.home_folder);
+   elm_object_part_content_set(pref_layout.general, "swallow.home", entry_home);
+   EWE_ENTRY_ADD(pref_layout.general, entry_swap, true, DEFAULT_STYLE);
+   elm_object_part_text_set(entry_swap, "elm.text", prof_general.swap_folder);
+   elm_object_part_content_set(pref_layout.general, "swallow.swap", entry_swap);
+}
+
+static void
+_workspace_form(Preferences *preference)
+{
+   Evas_Object *panes, *layout;
+   panes = evas_object_data_get(preference->mwin, "panes");
+   layout = elm_object_part_content_get(panes, "right");
+   if (layout)
+     evas_object_hide(layout);
+}
+
+#undef pref_layout
+#undef prof_general
+
+static void
+_on_glit_selected(void *data,
+                  Evas_Object *obj __UNUSED__,
+                  void *event_info)
+{
+   int count = 0;
+   Preferences *preference = (Preferences *)data;
+   Elm_Object_Item *glit = (Elm_Object_Item *)event_info;
+
+   count = elm_genlist_item_index_get(glit);
+   switch (count)
+     {
+      case 1:
+        {
+           _general_form(preference);
+           break;
+        }
+      case 2:
+        {
+           _workspace_form(preference);
+           break;
+        }
+     }
+
+}
+
+static Evas_Object *
+_form_left_side(Preferences *preference)
+{
+   Evas_Object *box;
+
+   if (!_itc)
+     {
+        _itc = elm_genlist_item_class_new();
+        _itc->item_style = "eflete/level1";
+        _itc->func.text_get = _item_label_get;
+        _itc->func.content_get = NULL;
+        _itc->func.state_get = NULL;
+        _itc->func.del = NULL;
+     }
+
+   BOX_ADD(preference->mwin, box, false, false);
+   preference->glist = elm_genlist_add(box);
+
+   preference->glist = elm_genlist_add(box);
+   elm_object_style_set(preference->glist, DEFAULT_STYLE);
+   elm_box_pack_end(box, preference->glist);
+   evas_object_show(preference->glist);
+
+   evas_object_size_hint_align_set(preference->glist, EVAS_HINT_FILL,
+                                   EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(preference->glist, EVAS_HINT_EXPAND,
+                                    EVAS_HINT_EXPAND);
+
+   elm_genlist_item_append(preference->glist, _itc,
+                           _("General"), NULL, ELM_GENLIST_ITEM_NONE,
+                           _on_glit_selected, preference);
+   elm_genlist_item_append(preference->glist, _itc,
+                           _("Workspace"), NULL, ELM_GENLIST_ITEM_NONE,
+                           _on_glit_selected, preference);
+   elm_genlist_item_append(preference->glist, _itc,
+                           _("Live view"), NULL, ELM_GENLIST_ITEM_NONE,
+                           _on_glit_selected, preference);
+
+   return box;
+}
+
 Evas_Object *
 preferences_window_add(Project *project)
 {
    Evas_Object *panes;
+   Evas_Object *layout_left;
    Evas_Object *window_layout, *button_box, *btn;
    Preferences *preference = NULL;
 
@@ -84,7 +237,14 @@ preferences_window_add(Project *project)
    evas_object_size_hint_align_set(panes, EVAS_HINT_FILL, EVAS_HINT_FILL);
    elm_panes_content_left_size_set(panes, 0.2);
    elm_object_part_content_set(window_layout, "eflete.swallow.content", panes);
+   evas_object_data_set(preference->mwin, "panes", panes);
    evas_object_show(panes);
+
+   layout_left = _form_left_side(preference);
+   elm_object_part_content_set(panes, "left", layout_left);
+   evas_object_show(layout_left);
+
+   elm_genlist_item_selected_set(elm_genlist_first_item_get(preference->glist), true);
 
    BOX_ADD(window_layout, button_box, true, false)
    elm_box_align_set(button_box, 1.0, 0.5);
