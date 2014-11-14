fbc66403e9f795efc9146998ffb75098f3eaabb9
Mykyta Biliavskyi <m.biliavskyi@samsung.com>
2014-11-14 20:15:51 +0200

    History: add support params, that represent with check control.
    Params like: event_propagation, mouse_events, scale and etc.

--- src/ui/blocks/ui_property_macro.h
+++ src/ui/blocks/ui_property_macro.h
@@ -138,8 +138,14 @@ _on_##SUB##_##VALUE##_change(void *data, \
 { \
    Prop_Data *pd = (Prop_Data *)data; \
    Eina_Bool value = elm_check_state_get(obj); \
+   Eina_Bool old_value = edje_edit_##SUB##_##VALUE##_get(pd->style->obj, \
+                                                         pd->part->name);\
    if (!edje_edit_##SUB##_##VALUE##_set(pd->style->obj, pd->part->name, value)) \
      return; \
+   history_diff_add(pd->style->obj, PROPERTY, MODIFY, INT, old_value, \
+                    value, pd->style->full_group_name,\
+                    (void*)edje_edit_##SUB##_##VALUE##_set,  #SUB"_"#VALUE, \
+                     pd->part->name, NULL, 0.0); \
    pm_project_changed(app_data_get()->project); \
    workspace_edit_object_recalc(pd->workspace); \
    pd->style->isModify = true; \
