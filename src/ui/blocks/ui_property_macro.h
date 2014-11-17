271f90b0fb32ccef5ed66506766f01c5a0660665
Mykyta Biliavskyi <m.biliavskyi@samsung.com>
2014-11-17 10:47:10 +0200

    History: add support textblock params.
    Support textblock params, that represent with combobox UI control.
    Params: select mode, entry mode, pointer mode, cursor mode.

--- src/ui/blocks/ui_property_macro.h
+++ src/ui/blocks/ui_property_macro.h
@@ -370,8 +370,14 @@ _on_##SUB##_##VALUE##_change(void *data, \
 { \
    Prop_Data *pd = (Prop_Data *)data; \
    Ewe_Combobox_Item *item = ei; \
+   int old_value = edje_edit_##SUB##_##VALUE##_get(pd->style->obj, pd->part->name); \
+   int value = item->index; \
    if (!edje_edit_##SUB##_##VALUE##_set(pd->style->obj, pd->part->name, (TYPE)item->index)) \
      return; \
+   history_diff_add(pd->style->obj, PROPERTY, MODIFY, INT, old_value, \
+                    value, pd->style->full_group_name,\
+                    (void*)edje_edit_##SUB##_##VALUE##_set,  #SUB"_"#VALUE, \
+                    pd->part->name, NULL, 0.0); \
    pm_project_changed(app_data_get()->project); \
    workspace_edit_object_recalc(pd->workspace); \
    pd->style->isModify = true; \
