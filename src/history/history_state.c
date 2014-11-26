f375f29a110418b626d6d55c2825914135c6fd68
Mykyta Biliavskyi <m.biliavskyi@samsung.com>
2014-11-26 13:30:36 +0200

    History: add support specifed state attributes for TEXT and IMAGE parts.

--- src/history/history_state.c
+++ src/history/history_state.c
@@ -21,7 +21,9 @@
 #include "history_private.h"
 
 typedef struct _State_Params State_Params;
-
+typedef struct _Description_Fill Description_Fill;
+typedef struct _Description_Image Description_Image;
+typedef struct _Description_Text Description_Text;
 /**
  * @struct _State_Diff
  *
@@ -39,6 +41,62 @@ struct _State_Diff
    Eina_Stringshare *part;  /**< Part name */
    Edje_Part_Type type; /**< Part type. Helper that make search needed union faster */
    State_Params *state; /**< Here stored all params of state */
+};
+
+struct _Description_Fill
+{
+   double         pos_rel_x; /* fill offset x relative to area */
+   double         rel_x; /* relative size compared to area */
+   double         pos_rel_y; /* fill offset y relative to area */
+   double         rel_y; /* relative size compared to area */
+   int            pos_abs_x; /* fill offset x added to fill offset */
+   int            abs_x; /* size of fill added to relative fill */
+   int            pos_abs_y; /* fill offset y added to fill offset */
+   int            abs_y; /* size of fill added to relative fill */
+   char           smooth; /* fill with smooth scaling or not */
+   unsigned char  type; /* fill coordinate from container (SCALE) or from source image (TILE) */
+};
+
+struct _Description_Image
+{
+   Description_Fill fill;
+   struct {
+      int l, r, t, b; /* border scaling on image fill */
+      unsigned char  no_fill; /* do we fill the center of the image if bordered? 1 == NO!!!! */
+   } border;
+
+   Eina_List *tweens;
+   Eina_Stringshare *normal;
+};
+
+struct _Description_Text
+{
+   Eina_Stringshare *text; /* if "" or NULL, then leave text unchanged */
+   Eina_Stringshare *text_class; /* how to apply/modify the font */
+   Eina_Stringshare *font; /* if a specific font is asked for */
+   Eina_Stringshare *repch;
+
+   struct {
+        float x, y;
+   } align; /* 0 <-> 1.0 alignment within allocated space */
+
+   struct {
+        int r, g, b, a;
+   } color3;
+
+   double         ellipsis; /* 0.0 - 1.0 defining where the ellipsis align */
+   int            size; /* 0 = use user set size */
+   Eina_Stringshare *source_part;
+   Eina_Stringshare *text_source_part;
+
+   unsigned char  fit_x; /* resize font size down to fit in x dir */
+   unsigned char  fit_y; /* resize font size down to fit in y dir */
+   unsigned char  min_x; /* if text size should be part min size */
+   unsigned char  min_y; /* if text size should be part min size */
+   unsigned char  max_x; /* if text size should be part max size */
+   unsigned char  max_y; /* if text size should be part max size */
+   int            size_range_min;
+   int            size_range_max; /* -1 means, no bound. */
 };
 
 struct _State_Params
@@ -78,6 +136,10 @@ struct _State_Params
       Eina_Stringshare *to_x; /* -1 = whole part collection, or part ID */
       Eina_Stringshare *to_y; /* -1 = whole part collection, or part ID */
    } rel1, rel2;
+   union {
+      Description_Text text;
+      Description_Image image;
+   };
 };
 
 static void
@@ -119,10 +181,11 @@ _history_ui_state_update(Evas_Object *source, State_Diff *change)
 /* argument 'type' will be used in future, for store special part types data */
 static State_Params *
 _state_params_save(Evas_Object *obj, const char *part, const char *state,
-                   double value, Edje_Part_Type type __UNUSED__)
+                   double value, Edje_Part_Type type)
 {
    State_Params *state_diff = NULL;
    int r, g, b, a;
+   int l, t;
 
    if ((!part) || (!state)) return NULL;
    if (!edje_edit_state_exist(obj, part, state, value)) return NULL;
@@ -178,14 +241,100 @@ _state_params_save(Evas_Object *obj, const char *part, const char *state,
    state_diff->color2.r = r; state_diff->color2.g = g;
    state_diff->color2.b = b; state_diff->color2.a = a;
 
+   switch (type)
+     {
+      case EDJE_PART_TYPE_IMAGE:
+         state_diff->image.fill.smooth =
+            edje_edit_state_fill_smooth_get(obj, part, state, value);
+         state_diff->image.fill.type =
+            edje_edit_state_fill_type_get(obj, part, state, value);
+         state_diff->image.fill.pos_rel_x =
+            edje_edit_state_fill_origin_relative_x_get(obj, part, state, value);
+         state_diff->image.fill.pos_rel_y =
+            edje_edit_state_fill_origin_relative_y_get(obj, part, state, value);
+         state_diff->image.fill.pos_abs_x =
+            edje_edit_state_fill_origin_offset_x_get(obj, part, state, value);
+         state_diff->image.fill.pos_abs_y =
+            edje_edit_state_fill_origin_offset_y_get(obj, part, state, value);
+         state_diff->image.fill.rel_x =
+            edje_edit_state_fill_size_relative_x_get(obj, part, state, value);
+         state_diff->image.fill.rel_y =
+            edje_edit_state_fill_size_relative_y_get(obj, part, state, value);
+         state_diff->image.fill.abs_x =
+            edje_edit_state_fill_size_offset_x_get(obj, part, state, value);
+         state_diff->image.fill.abs_y =
+            edje_edit_state_fill_size_offset_y_get(obj, part, state, value);
+
+         state_diff->image.border.no_fill =
+            edje_edit_state_image_border_fill_get(obj, part, state, value);
+         edje_edit_state_image_border_get(obj, part, state, value, &l, &r, &t, &b);
+         state_diff->image.border.l = l; state_diff->image.border.r = r;
+         state_diff->image.border.t = t; state_diff->image.border.b = b;
+         state_diff->image.border.t = t; state_diff->image.border.b = b;
+
+         state_diff->image.tweens =
+            edje_edit_state_tweens_list_get(obj, part, state, value);
+         state_diff->image.normal =
+            edje_edit_state_image_get(obj, part, state, value);
+      break;
+      case EDJE_PART_TYPE_TEXT:
+         state_diff->text.text =
+            eina_stringshare_add(edje_edit_state_text_get(obj, part, state, value));
+         state_diff->text.font =
+            eina_stringshare_add(edje_edit_state_font_get(obj, part, state, value));
+         state_diff->text.text_source_part =
+            eina_stringshare_add(edje_edit_state_text_text_source_get(obj, part,
+                                                                  state, value));
+         state_diff->text.source_part =
+            eina_stringshare_add(edje_edit_state_text_source_get(obj, part,
+                                                                 state, value));
+         state_diff->text.text_class =
+            eina_stringshare_add(edje_edit_state_text_class_get(obj, part,
+                                                                state, value));
+         state_diff->text.repch =
+            eina_stringshare_add(edje_edit_state_text_repch_get(obj, part,
+                                                                state, value));
+         edje_edit_state_text_size_range_min_max_get(obj, part, state, value,
+                                                     &state_diff->text.size_range_min,
+                                                     &state_diff->text.size_range_max);
+
+         state_diff->text.size =
+            edje_edit_state_text_size_get(obj, part, state, value);
+         state_diff->text.align.x =
+            edje_edit_state_text_align_x_get(obj, part, state, value);
+         state_diff->text.align.y =
+            edje_edit_state_text_align_y_get(obj, part, state, value);
+         state_diff->text.ellipsis =
+            edje_edit_state_text_elipsis_get(obj, part, state, value);
+         state_diff->text.fit_x =
+            edje_edit_state_text_fit_x_get(obj, part, state, value);
+         state_diff->text.fit_y =
+            edje_edit_state_text_fit_y_get(obj, part, state, value);
+         state_diff->text.min_x =
+            edje_edit_state_text_min_x_get(obj, part, state, value);
+         state_diff->text.min_y =
+            edje_edit_state_text_min_y_get(obj, part, state, value);
+         state_diff->text.max_x =
+            edje_edit_state_text_max_x_get(obj, part, state, value);
+         state_diff->text.max_y =
+            edje_edit_state_text_max_y_get(obj, part, state, value);
+
+      break;
+      default:
+      break;
+     }
+
    return state_diff;
 }
 
 /* argument 'type' will be used in future, for restore special part types data */
 static Eina_Bool
 _state_param_restore(Evas_Object *obj, Eina_Stringshare *part,
-                     State_Params *state_diff, Edje_Part_Type type __UNUSED__)
+                     State_Params *state_diff, Edje_Part_Type type)
 {
+   Eina_List *l, *l_next;
+   Eina_Stringshare *tween = NULL;
+
    if ((!part) || (!state_diff)) return false;
 
    if (!edje_edit_state_exist(obj, part, state_diff->name, state_diff->value))
@@ -260,6 +409,114 @@ _state_param_restore(Evas_Object *obj, Eina_Stringshare *part,
    edje_edit_state_rel2_relative_y_set(obj, part, state_diff->name,
                                        state_diff->value,
                                        state_diff->rel2.relative_y);
+   switch (type)
+     {
+      case EDJE_PART_TYPE_IMAGE:
+         edje_edit_state_fill_smooth_set(obj, part, state_diff->name,
+                                         state_diff->value,
+                                         state_diff->image.fill.smooth);
+         edje_edit_state_fill_type_set(obj, part, state_diff->name,
+                                       state_diff->value,
+                                       state_diff->image.fill.type);
+         edje_edit_state_fill_origin_relative_x_set(obj, part, state_diff->name,
+                                                    state_diff->value,
+                                                    state_diff->image.fill.pos_rel_x);
+         edje_edit_state_fill_origin_relative_y_set(obj, part, state_diff->name,
+                                                    state_diff->value,
+                                                    state_diff->image.fill.pos_rel_y);
+         edje_edit_state_fill_origin_offset_x_set(obj, part, state_diff->name,
+                                                  state_diff->value,
+                                                  state_diff->image.fill.pos_abs_x);
+         edje_edit_state_fill_origin_offset_y_set(obj, part, state_diff->name,
+                                                  state_diff->value,
+                                                  state_diff->image.fill.pos_abs_y);
+         edje_edit_state_fill_size_relative_x_set(obj, part, state_diff->name,
+                                                  state_diff->value,
+                                                  state_diff->image.fill.rel_x);
+         edje_edit_state_fill_size_relative_y_set(obj, part, state_diff->name,
+                                                  state_diff->value,
+                                                  state_diff->image.fill.rel_y);
+         edje_edit_state_fill_size_offset_x_set(obj, part, state_diff->name,
+                                                state_diff->value,
+                                                state_diff->image.fill.abs_x);
+         edje_edit_state_fill_size_offset_y_set(obj, part, state_diff->name,
+                                                state_diff->value,
+                                                state_diff->image.fill.abs_y);
+
+         edje_edit_state_image_border_fill_set(obj, part, state_diff->name,
+                                               state_diff->value,
+                                               state_diff->image.border.no_fill);
+         edje_edit_state_image_border_set(obj, part, state_diff->name,
+                                          state_diff->value,
+                                          state_diff->image.border.l,
+                                          state_diff->image.border.r,
+                                          state_diff->image.border.t,
+                                          state_diff->image.border.b);
+
+         EINA_LIST_FOREACH_SAFE(state_diff->image.tweens, l, l_next, tween)
+           {
+              edje_edit_state_tween_add(obj, part, state_diff->name,
+                                        state_diff->value, tween);
+           }
+
+         edje_edit_state_image_set(obj, part, state_diff->name,
+                                   state_diff->value, state_diff->image.normal);
+      break;
+      case EDJE_PART_TYPE_TEXT:
+         edje_edit_state_text_set(obj, part, state_diff->name,
+                                  state_diff->value, state_diff->text.text);
+         edje_edit_state_font_set(obj, part, state_diff->name,
+                                  state_diff->value, state_diff->text.font);
+         edje_edit_state_text_text_source_set(obj, part, state_diff->name,
+                                              state_diff->value,
+                                              state_diff->text.text_source_part);
+         edje_edit_state_text_source_set(obj, part, state_diff->name,
+                                         state_diff->value,
+                                         state_diff->text.source_part);
+         edje_edit_state_text_class_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.text_class);
+         edje_edit_state_text_repch_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.repch);
+         edje_edit_state_text_size_range_min_max_set(obj, part, state_diff->name,
+                                                     state_diff->value,
+                                                     state_diff->text.size_range_min,
+                                                     state_diff->text.size_range_max);
+
+         edje_edit_state_text_size_set(obj, part, state_diff->name,
+                                       state_diff->value, state_diff->text.size);
+         edje_edit_state_text_align_x_set(obj, part, state_diff->name,
+                                          state_diff->value,
+                                          state_diff->text.align.x);
+         edje_edit_state_text_align_y_set(obj, part, state_diff->name,
+                                          state_diff->value,
+                                          state_diff->text.align.y);
+         edje_edit_state_text_elipsis_set(obj, part, state_diff->name,
+                                          state_diff->value,
+                                          state_diff->text.ellipsis);
+         edje_edit_state_text_fit_x_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.fit_x);
+         edje_edit_state_text_fit_y_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.fit_y);
+         edje_edit_state_text_min_x_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.min_x);
+         edje_edit_state_text_min_y_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.min_y);
+         edje_edit_state_text_max_x_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.max_x);
+         edje_edit_state_text_max_y_set(obj, part, state_diff->name,
+                                        state_diff->value,
+                                        state_diff->text.max_y);
+      break;
+      default:
+      break;
+     }
    return true;
 }
 
