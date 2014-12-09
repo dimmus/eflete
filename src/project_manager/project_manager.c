008a34beb5239c24a40bd5475d4269a01b0a3519
Igor Gala <i.gala@samsung.com>
2014-12-09 14:37:44 +0200

    Project manager: change malloc to calloc
    It was reciewed segmentation fault on all what was used
    project->current_style.

--- src/project_manager/project_manager.c
+++ src/project_manager/project_manager.c
@@ -34,7 +34,7 @@ static Eet_Compression compess_level = EET_COMPRESSION_HI;
 #define WORKER_CREATE(FUNC_PROGRESS, FUNC_END, DATA, PROJECT, \
                       NAME, PATH, EDJ, EDC, BUILD_OPTIONS) \
 { \
-   worker = (Project_Thread *)mem_malloc(sizeof(Project_Thread)); \
+   worker = (Project_Thread *)mem_calloc(1, sizeof(Project_Thread)); \
    worker->func_progress = FUNC_PROGRESS; \
    worker->func_end = FUNC_END; \
    worker->data = (void *)DATA; \
@@ -191,16 +191,12 @@ _project_files_create(Project_Thread *worker)
    if (error) return NULL;
 
    THREAD_TESTCANCEL;
-   pro = (Project *)mem_malloc(sizeof(Project));
+   pro = (Project *)mem_calloc(1, sizeof(Project));
    WORKER_LOCK_TAKE;
       folder_path = eina_stringshare_printf("%s/%s", worker->path, worker->name);
       pro->name = eina_stringshare_add(worker->name);
       pro->dev = eina_stringshare_printf("%s/%s.dev", folder_path, worker->name);
       pro->develop_path = eina_stringshare_printf("%s/develop", folder_path);
-      pro->release_options = NULL;
-      pro->changed = false;
-      pro->close_request = false;
-      pro->added_sounds = NULL;
 
       pro_path = eina_stringshare_printf("%s/%s.pro", folder_path, worker->name);
       ecore_file_mkdir(pro->develop_path);
