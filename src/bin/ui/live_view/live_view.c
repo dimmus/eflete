d09bcda173369764681064b5a091f85889f2755b
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2015-02-12 09:27:36 +0200

    Styles: move the Live View layouts default eflete theme

--- src/bin/ui/live_view/live_view.c
+++ src/bin/ui/live_view/live_view.c
@@ -39,7 +39,7 @@ live_view_add(Evas_Object *parent, Eina_Bool in_prog_edit)
    live->in_prog_edit = in_prog_edit;
 
    live->layout = elm_layout_add(parent);
-   elm_layout_file_set(live->layout, EFLETE_EDJ, "eflete/live_view/toolbar/default");
+   elm_layout_theme_set(live->layout, "layout", "live_view", "toolbar");
    bg = elm_bg_add(live->layout);
    elm_bg_color_set(bg, 203, 207, 209);
    evas_object_show(bg);
