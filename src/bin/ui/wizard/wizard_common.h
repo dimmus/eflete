838512f0da5d08043f46f861ababf272454260ed
Igor Gala <i.gala@samsung.com>
2015-03-03 16:51:25 +0200

    Wizard: remove unusable macros (modal window changed to inwin)

--- src/bin/ui/wizard/wizard_common.h
+++ src/bin/ui/wizard/wizard_common.h
@@ -1,50 +1,5 @@
 #include "main_window.h"
 
-#define FILESELCTOR_WINDOW(FUNC, TITLE, FOLDER_ONLY, FILTER, FILTER_CB, FIELD) \
-static void \
-FUNC##_done(void *data, \
-            Evas_Object *obj __UNUSED__, \
-            void *event_info) \
-{ \
-   Wizard_Import_Edj_Win *wiew; \
-   const char *selected; \
-\
-   selected = event_info; \
-   wiew = (Wizard_Import_Edj_Win *)data; \
-   if ((selected) && (strcmp(selected, ""))) \
-     elm_entry_entry_set(wiew->FIELD, selected); \
-\
-   _fs_close(NULL, wiew->fs, NULL); \
-} \
-\
-static void \
-FUNC(void *data, \
-     Evas_Object *obj __UNUSED__, \
-     void *event_info __UNUSED__) \
-{ \
-   Evas_Object *bg, *fs; \
-   Wizard_Import_Edj_Win *wiew; \
-   const char *path; \
-\
-   wiew = (Wizard_Import_Edj_Win *)data; \
-\
-   MODAL_WINDOW_ADD(wiew->fs, main_window_get(), TITLE, _fs_close, NULL); \
-   bg = elm_bg_add(wiew->fs); \
-   evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND); \
-   evas_object_show(bg); \
-   elm_win_resize_object_add(wiew->fs, bg); \
-   FILESELECTOR_ADD(fs, wiew->fs, FUNC##_done, data); \
-   elm_fileselector_folder_only_set(fs, FOLDER_ONLY); \
-   path = elm_entry_entry_get(wiew->FIELD); \
-   if ((path) && (ecore_file_is_dir(path))) elm_fileselector_path_set(fs, path); \
-   if (FILTER) \
-     { \
-        elm_fileselector_custom_filter_append(fs, FILTER_CB, NULL, "Edje Files"); \
-        elm_fileselector_mime_types_filter_append(fs, "*", "All Files"); \
-     } \
-   elm_win_resize_object_add(wiew->fs, fs); \
-}
-
 #define FILESELCTOR_INWIN(FUNC, TITLE, FOLDER_ONLY, FILTER, FILTER_CB, FIELD) \
 static void \
 FUNC##_done(void *data, \
