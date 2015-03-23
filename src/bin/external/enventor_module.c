81f93af14061922c8f386c073fa144ddc9d9ec4f
Mykyta Biliavskyi <m.biliavskyi@samsung.net>
2015-03-23 13:12:49 +0200

    Enventor: refactoring, isolate manipulation with enventor.
    Create enventor module files, that will contain API and internal
    functions for using enventor in eflete.

---
+++ src/bin/external/enventor_module.c
@@ -0,0 +1,30 @@
+/*
+ * Edje Theme Editor
+ * Copyright (C) 2013-2015 Samsung Electronics.
+ *
+ * This file is part of Edje Theme Editor.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see www.gnu.org/licenses/lgpl.html.
+ */
+
+#include "enventor_module.h"
+
+#ifdef HAVE_ENVENTOR
+
+Evas_Object *
+enventor_object_init(Evas_Object *parent)
+{
+  return enventor_object_add(parent);
+}
+
+#endif /* HAVE_ENVENTOR */
