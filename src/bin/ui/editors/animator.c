894bbd35a30902ceb09b377f5422db11bb8dd662
Vitalii Vorobiov <vi.vorobiov@samsung.com>
2015-04-16 13:19:51 +0300

    Live View: update live view when new signal added
    @fix #41849

--- src/bin/ui/editors/animator.c
+++ src/bin/ui/editors/animator.c
@@ -175,6 +175,7 @@ _on_animator_save(void *data,
    ui_signal_list_data_unset(ui_block_signal_list_get(ap));
    ui_signal_list_data_set(ui_block_signal_list_get(ap), style);
    edje_edit_without_source_save(style->obj, true);
+   live_view_widget_style_set(ap->live_view, ap->project, style);
    project_changed();
 }
 
