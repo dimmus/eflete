f91db1452c5c09e8a3d47d5a57b30bb548debcc4
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2015-04-20 15:55:11 +0300

    Property: refactor the text source of TEXT part attribute control
    Change-Id: I67934a0dbf0d501325892e55cb3d722986b7e3a2

--- src/bin/ui/blocks/ui_property.c
+++ src/bin/ui/blocks/ui_property.c
@@ -1841,8 +1841,9 @@ STATE_ATTR_COMBOBOX(_("proxy source"), state_text, source, state_text, EDJE_PART
                     _("Causes the part to use the text properties "
                       "(like font and size) of another part "
                       "and update them as they change."))
-ITEM_1COMBOBOX_STATE_CREATE(TEXT_SOURCE, _("text source"), state_text, text_source, styles)
-
+STATE_ATTR_COMBOBOX(_("proxy source"), state_text, text_source, state_text, EDJE_PART_TYPE_TEXT, ==,
+                    _("Causes the part to display the text content of "
+                      "another part and update them as they change."));
 
 static Eina_Bool
 _text_effect_get(Prop_Data *pd, int *type, int *direction)
@@ -2205,10 +2206,8 @@ ui_property_state_text_set(Evas_Object *property)
          elm_box_pack_end(box, item);
          item = prop_state_text_source_add(box, pd);
          elm_box_pack_end(box, item);
-         pd_text.text_source = prop_item_state_text_text_source_add(box, pd, NULL,
-                               _("Causes the part to display the text content of \t"
-                               "another part and update them as they change."));
-         elm_box_pack_end(box, pd_text.text_source);
+         item = prop_state_text_text_source_add(box, pd);
+         elm_box_pack_end(box, item);
          pd_text.ellipsis = prop_item_state_text_ellipsis_add(box, pd,
                             _("Cut text if biggest then part's area"
                             "0.0 = fix the left side  1.0 = right side"));
@@ -2234,7 +2233,7 @@ ui_property_state_text_set(Evas_Object *property)
         STATE_ATTR_2CHECK_UPDATE(state_text, min_x, min_y, state_text)
         STATE_ATTR_2CHECK_UPDATE(state_text, max_x, max_y, state_text)
         prop_state_text_source_update(pd);
-        prop_item_state_text_text_source_update(pd_text.text_source, pd);
+        prop_state_text_text_source_update(pd);
         prop_item_state_text_ellipsis_update(pd_text.ellipsis, pd);
         STATE_ATTR_2CHECK_UPDATE(state_text, fit_x, fit_y, state_text)
         prop_state_color2_update(pd);
