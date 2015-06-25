1f266d167395cdaaabd97c4480f4774f0f06cb52
Vitalii Vorobiov <vi.vorobiov@samsung.com>
2015-06-25 16:00:16 +0300

    Colorclass Manager: show modal window
    Empty for now.
    Next, it will show list of colorclasses.

--- src/bin/ui/editors/colorclass_editor.c
+++ src/bin/ui/editors/colorclass_editor.c
@@ -22,8 +22,69 @@
 
 TODO("Rename this file to colorclass_manager")
 
-Evas_Object *
-colorclass_viewer_add(Project *project __UNUSED__)
+
+typedef struct _Colorclasses_Editor Colorclasses_Editor;
+struct _Colorclasses_Editor
 {
-   return NULL;
+   Project *pr;
+   Evas_Object *mwin;
+   Eina_Bool changed;
+};
+
+static void
+_on_mwin_del(void * data,
+             Evas *e __UNUSED__,
+             Evas_Object *obj __UNUSED__,
+             void *event_info __UNUSED__)
+{
+   App_Data *ap = (App_Data *)data;
+   ui_menu_items_list_disable_set(ap->menu, MENU_ITEMS_LIST_MAIN, false);
+   ap->modal_editor--;
+}
+
+static void
+_on_btn_cancel(void *data,
+               Evas_Object *obj __UNUSED__,
+               void *event_info __UNUSED__)
+{
+   Colorclasses_Editor *edit = (Colorclasses_Editor *)data;
+   mw_del(edit->mwin);
+}
+
+Evas_Object *
+colorclass_viewer_add(Project *project)
+{
+   Evas_Object *ic;
+   Colorclasses_Editor *edit = NULL;
+   /* temporary solution, while it not moved to modal window */
+   App_Data *ap = app_data_get();
+
+   if (!project)
+     {
+        ERR("Project isn't opened");
+        return NULL;
+     }
+
+   edit = (Colorclasses_Editor *)mem_calloc(1, sizeof(Colorclasses_Editor));
+   edit->changed = false;
+   edit->pr = project;
+   edit->mwin = mw_add(_on_btn_cancel, edit);
+   if (!edit->mwin)
+     {
+        free(edit);
+        return NULL;
+     }
+   mw_title_set(edit->mwin, _("Color class manager"));
+   ic = elm_icon_add(edit->mwin);
+   elm_icon_standard_set(ic, "color");
+   mw_icon_set(edit->mwin, ic);
+
+   ui_menu_items_list_disable_set(ap->menu, MENU_ITEMS_LIST_MAIN, true);
+   evas_object_event_callback_add(edit->mwin, EVAS_CALLBACK_DEL, _on_mwin_del, ap);
+
+   evas_object_show(edit->mwin);
+
+   ap->modal_editor++;
+
+   return edit->mwin;
 }
