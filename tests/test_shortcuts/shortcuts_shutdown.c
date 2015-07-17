8aed8e8998fd1587f9f8f2a9f889f6e9ff23332e
Andrii Kroitor <an.kroitor@samsung.com>
2015-07-17 11:10:47 +0300

    UTC: fix shortcuts tests

--- tests/test_shortcuts/shortcuts_shutdown.c
+++ tests/test_shortcuts/shortcuts_shutdown.c
@@ -59,7 +59,6 @@ EFLETE_TEST(shortcuts_shutdown_test_p1)
    shortcuts_init(ap);
    ck_assert_msg(shortcuts_shutdown(ap), "Can't shutdown shortcuts.");
 
-   shortcuts_shutdown(ap);
    elm_shutdown();
 }
 END_TEST
@@ -97,101 +96,6 @@ EFLETE_TEST(shortcuts_shutdown_test_p2)
    ck_assert_msg(shortcuts_shutdown(ap), "Can't shutdown shortcuts.");
    shortcuts_init(ap);
    ck_assert_msg(shortcuts_shutdown(ap), "Can't shutdown shortcuts after second initialization.");
-
-   elm_shutdown();
-}
-END_TEST
-
-/**
- * @addtogroup shortcuts_shutdown
- * @{
- * <tr>
- * <td>shortcuts_shutdown</td>
- * <td>shortcuts_shutdown_test_n1</td>
- * <td>
- * @precondition
- * @step 1 initialize elementary library
- * @step 2 create App_Data
- *
- * @procedure
- * @step 1 call shortcuts_shutdown
- * </td>
- * <td>App_Data *ap</td>
- * <td>EINA_FALSE</td>
- * </tr>
- * @}
- */
-EFLETE_TEST(shortcuts_shutdown_test_n1)
-{
-   App_Data *ap;
-
-   elm_init(0,0);
-   ap = app_data_get();
-
-   ck_assert_msg(!shortcuts_shutdown(ap), "Shutdown not initialized shortcuts was successfull but should not!");
-
-   elm_shutdown();
-}
-END_TEST
-
-/**
- * @addtogroup shortcuts_shutdown
- * @{
- * <tr>
- * <td>shortcuts_shutdown</td>
- * <td>shortcuts_shutdown_test_n2</td>
- * <td>
- * @precondition
- * @step 1 initialize elementary library
- * @step 2 create App_Data
- *
- * @procedure
- * @step 1 call shortcuts_shutdown
- * @step 2 call shortcuts_shutdown
- * </td>
- * <td>App_Data *ap</td>
- * <td>EINA_FALSE</td>
- * </tr>
- * @}
- */
-EFLETE_TEST(shortcuts_shutdown_test_n2)
-{
-   App_Data *ap;
-
-   elm_init(0,0);
-   ap = app_data_get();
-
-   shortcuts_init(ap);
-   ck_assert_msg(shortcuts_shutdown(ap), "Can't shutdown shortcuts.");
-   ck_assert_msg(!shortcuts_shutdown(ap), "Shutdown not initialized shortcuts was successfull but should not!");
-
-   elm_shutdown();
-}
-END_TEST
-
-/**
- * @addtogroup shortcuts_shutdown
- * @{
- * <tr>
- * <td>shortcuts_shutdown</td>
- * <td>shortcuts_shutdown_test_n3</td>
- * <td>
- * @precondition
- * @step 1 initialize elementary library
- *
- * @procedure
- * @step 1 call shortcuts_shutdown
- * </td>
- * <td>NULL</td>
- * <td>EINA_FALSE</td>
- * </tr>
- * @}
- */
-EFLETE_TEST(shortcuts_shutdown_test_n3)
-{
-   elm_init(0,0);
-
-   ck_assert_msg(!shortcuts_shutdown(NULL), "Shutdown was successfull but should not!");
 
    elm_shutdown();
 }
