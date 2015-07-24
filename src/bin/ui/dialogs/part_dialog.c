fadf269a416c231963069587b43e2e83aad48e29
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2015-07-24 09:22:08 +0300

    Part dialog: free validator before close the dialog
    @fix #45627
    
    Change-Id: I370728dfb05bbc600fe2ab34e0a0e91553524a94

--- src/bin/ui/dialogs/part_dialog.c
+++ src/bin/ui/dialogs/part_dialog.c
@@ -79,6 +79,7 @@ _cancel_clicked(void *data,
    ap->popup = NULL; \
    ui_menu_items_list_disable_set(ap->menu, MENU_ITEMS_LIST_MAIN, false); \
    live_view_part_add(ap->live_view, ui_widget_list_selected_part_get(widget_list)); \
+   ecore_job_add(_job_popup_del, data); \
    project_changed(true);
 
 static void
