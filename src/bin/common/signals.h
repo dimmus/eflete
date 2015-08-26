96fb7de82dba395afaff2474cc5ae97ce009904f
Andrii Kroitor <an.kroitor@samsung.com>
2015-08-26 12:02:27 +0300

    common: add header for signals

---
+++ src/bin/common/signals.h
@@ -0,0 +1,44 @@
+/*
+ * Edje Theme Editor
+ * Copyright (C) 2013-2014 Samsung Electronics.
+ *
+ * This file is part of Edje Theme Editor.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see www.gnu.org/licenses/lgpl.html.
+ */
+
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+/* emited when part is selected.
+   eventinfo - pointer to Part_ structure */
+#define SIGNAL_PART_SELECTED "SIGNAL_PART_SELECTED"
+
+/* emited when part is unselected.
+   eventinfo - pointer to Part_ structure */
+#define SIGNAL_PART_UNSELECTED "SIGNAL_PART_UNSELECTED"
+
+/* emited when part state changed.
+   eventinfo - pointer to Part_ structure */
+#define SIGNAL_PART_STATE_CHANGED "SIGNAL_PART_STATE_CHANGED"
+
+/* emited when part item is selected.
+   eventinfo - pointer to Part_ structure */
+#define SIGNAL_PART_ITEM_SELECTED "SIGNAL_PART_ITEM_SELECTED"
+
+/* emited when part item is unselected.
+   eventinfo - pointer to Part_ structure */
+#define SIGNAL_PART_ITEM_UNSELECTED "SIGNAL_PART_ITEM_UNSELECTED"
+
+
+#endif /* SIGNALS_H*/
