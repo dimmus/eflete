7c7a500b858c4fdd78180b8d88ec92ada5670df6
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2015-10-08 11:38:44 +0300

    tab_home_common: fix the action popup title
    Change-Id: I26ed6eff0f59a464b5b3db2cf3fd1829b76f0b47

--- src/bin/ui/tab_home_common.c
+++ src/bin/ui/tab_home_common.c
@@ -155,7 +155,7 @@ exist_permission_check(const char *path, const char *name, const char *title)
      {
         eina_strbuf_reset(buf_msg);
         eina_strbuf_append_printf(buf_msg, _("Haven't permision to overwrite '%s' in '%s'"), name, path);
-        popup_want_action("New project", eina_strbuf_string_get(buf_msg), NULL, NULL, BTN_OK, NULL, NULL);
+        popup_want_action(title, eina_strbuf_string_get(buf_msg), NULL, NULL, BTN_OK, NULL, NULL);
      }
    ecore_file_recursive_rm(eina_strbuf_string_get(buf));
    eina_strbuf_free(buf_msg);
