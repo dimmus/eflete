5564ec1197d238ecb5fa5ead621527f4dfcfade9
Andrii Kroitor <an.kroitor@samsung.com>
2015-11-09 18:41:36 +0200

    sound_editor: remove old code (CID 85131)

--- src/bin/ui/editors/sound_editor.c
+++ src/bin/ui/editors/sound_editor.c
@@ -1265,7 +1265,6 @@ _add_tone_done(void *data,
                Evas_Object *obj __UNUSED__,
                void *event_info __UNUSED__)
 {
-   Sound *snd;
    Item *it;
    Eina_Stringshare *tone_name;
    const char *str_value;
@@ -1297,9 +1296,6 @@ _add_tone_done(void *data,
    str_value = elm_entry_entry_get(edit->frq_entry);
    frq = atoi(str_value);
 
-   snd = (Sound *)mem_calloc(1, sizeof(Sound));
-   snd->name = tone_name;
-   snd->tone_frq = frq;
    it = (Item *)mem_calloc(1, sizeof(Item));
    it->sound_name = eina_stringshare_add(tone_name);
    it->tone_frq = frq;
@@ -1311,7 +1307,7 @@ _add_tone_done(void *data,
    tone->freq = frq;
    ap.project->tones = eina_list_sorted_insert(ap.project->tones, (Eina_Compare_Cb) resource_cmp, tone);
 
-   edje_edit_sound_tone_add(ap.project->global_object, snd->name, snd->tone_frq);
+   edje_edit_sound_tone_add(ap.project->global_object, tone_name, frq);
    editor_save(ap.project->global_object);
    TODO("Remove this line once edje_edit_image_add would be added into Editor Module and saving would work properly")
    ap.project->changed = true;
