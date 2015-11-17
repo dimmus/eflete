d3cc6ee1272a419ba338ef92d1a53c3238c50586
Andrii Kroitor <an.kroitor@samsung.com>
2015-11-17 14:37:19 +0200

    editor: remove unnecessary macro duplicates

--- src/bin/editor/default.c
+++ src/bin/editor/default.c
@@ -31,7 +31,7 @@ editor_##FUNC##_default_is(Evas_Object *edit_object, PROTO_ARGS) \
    return !(fabs(val - DEF_VAL) > DBL_EPSILON); \
 }
 
-#define EDITOR_DOUBLE_RESET(FUNC, PROTO_ARGS, ARGS, RESET_VAL) \
+#define EDITOR_RESET(FUNC, PROTO_ARGS, ARGS, RESET_VAL) \
 Eina_Bool \
 editor_##FUNC##_reset(Evas_Object *edit_object, Change *change, PROTO_ARGS) \
 { \
@@ -42,7 +42,7 @@ editor_##FUNC##_reset(Evas_Object *edit_object, Change *change, PROTO_ARGS) \
 
 #define EDITOR_STATE_DOUBLE_RESET(FUNC, DEF_VAL) \
 EDITOR_DOUBLE_DEFAULT_CHECK(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL) \
-EDITOR_DOUBLE_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL)
+EDITOR_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL)
 
 EDITOR_STATE_DOUBLE_RESET(rel1_relative_x, 0.0)
 EDITOR_STATE_DOUBLE_RESET(rel2_relative_x, 1.0)
@@ -64,78 +64,46 @@ EDITOR_STATE_DOUBLE_RESET(text_align_x, 0.5)
 EDITOR_STATE_DOUBLE_RESET(text_align_y, 0.5)
 EDITOR_STATE_DOUBLE_RESET(text_elipsis, 0.0)
 
-#define EDITOR_INT_DEFAULT_CHECK(FUNC, PROTO_ARGS, ARGS, DEF_VAL) \
+#define EDITOR_SIMPLE_DEFAULT_CHECK(FUNC, PROTO_ARGS, ARGS, DEF_VAL) \
 Eina_Bool \
 editor_##FUNC##_default_is(Evas_Object *edit_object, PROTO_ARGS) \
 { \
    assert(edit_object != NULL); \
-   int val = edje_edit_##FUNC##_get(edit_object, ARGS); \
-   return val == DEF_VAL; \
+   return (edje_edit_##FUNC##_get(edit_object, ARGS) == DEF_VAL); \
 }
 
-#define EDITOR_INT_RESET(FUNC, PROTO_ARGS, ARGS, RESET_VAL) \
-Eina_Bool \
-editor_##FUNC##_reset(Evas_Object *edit_object, Change *change, PROTO_ARGS) \
-{ \
-   assert(edit_object != NULL); \
-   if (editor_##FUNC##_default_is(edit_object, ARGS)) return true; \
-   return editor_##FUNC##_set(edit_object, change, false, ARGS, RESET_VAL); \
-}
+#define EDITOR_STATE_SIMPLE_RESET(FUNC, DEF_VAL) \
+EDITOR_SIMPLE_DEFAULT_CHECK(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL) \
+EDITOR_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL)
 
-#define EDITOR_STATE_INT_RESET(FUNC, DEF_VAL) \
-EDITOR_INT_DEFAULT_CHECK(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL) \
-EDITOR_INT_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL)
+EDITOR_STATE_SIMPLE_RESET(rel1_offset_x, 0)
+EDITOR_STATE_SIMPLE_RESET(rel1_offset_y, 0)
+EDITOR_STATE_SIMPLE_RESET(rel2_offset_x, -1)
+EDITOR_STATE_SIMPLE_RESET(rel2_offset_y, -1)
+EDITOR_STATE_SIMPLE_RESET(max_w, -1)
+EDITOR_STATE_SIMPLE_RESET(max_h, -1)
+EDITOR_STATE_SIMPLE_RESET(min_w, 0)
+EDITOR_STATE_SIMPLE_RESET(min_h, 0)
+EDITOR_STATE_SIMPLE_RESET(fill_origin_offset_x, 0)
+EDITOR_STATE_SIMPLE_RESET(fill_origin_offset_y, 0)
+EDITOR_STATE_SIMPLE_RESET(fill_size_offset_x, -1)
+EDITOR_STATE_SIMPLE_RESET(fill_size_offset_y, -1)
+EDITOR_STATE_SIMPLE_RESET(container_padding_x, 0)
+EDITOR_STATE_SIMPLE_RESET(container_padding_y, 0)
+EDITOR_STATE_SIMPLE_RESET(text_size, 0)
 
-EDITOR_STATE_INT_RESET(rel1_offset_x, 0)
-EDITOR_STATE_INT_RESET(rel1_offset_y, 0)
-EDITOR_STATE_INT_RESET(rel2_offset_x, -1)
-EDITOR_STATE_INT_RESET(rel2_offset_y, -1)
-EDITOR_STATE_INT_RESET(max_w, -1)
-EDITOR_STATE_INT_RESET(max_h, -1)
-EDITOR_STATE_INT_RESET(min_w, 0)
-EDITOR_STATE_INT_RESET(min_h, 0)
-EDITOR_STATE_INT_RESET(fill_origin_offset_x, 0)
-EDITOR_STATE_INT_RESET(fill_origin_offset_y, 0)
-EDITOR_STATE_INT_RESET(fill_size_offset_x, -1)
-EDITOR_STATE_INT_RESET(fill_size_offset_y, -1)
-EDITOR_STATE_INT_RESET(container_padding_x, 0)
-EDITOR_STATE_INT_RESET(container_padding_y, 0)
-EDITOR_STATE_INT_RESET(text_size, 0)
-
-#define EDITOR_BOOL_DEFAULT_CHECK(FUNC, PROTO_ARGS, ARGS, DEF_VAL) \
-Eina_Bool \
-editor_##FUNC##_default_is(Evas_Object *edit_object, PROTO_ARGS) \
-{ \
-   assert(edit_object != NULL); \
-   Eina_Bool val = edje_edit_##FUNC##_get(edit_object, ARGS); \
-   return val == DEF_VAL; \
-}
-
-#define EDITOR_BOOL_RESET(FUNC, PROTO_ARGS, ARGS, RESET_VAL) \
-Eina_Bool \
-editor_##FUNC##_reset(Evas_Object *edit_object, Change *change, PROTO_ARGS) \
-{ \
-   assert(edit_object != NULL); \
-   if (editor_##FUNC##_default_is(edit_object, ARGS)) return true; \
-   return editor_##FUNC##_set(edit_object, change, false, ARGS, RESET_VAL); \
-}
-
-#define EDITOR_STATE_BOOL_RESET(FUNC, DEF_VAL) \
-EDITOR_BOOL_DEFAULT_CHECK(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL) \
-EDITOR_BOOL_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL)
-
-EDITOR_STATE_BOOL_RESET(fixed_h, false)
-EDITOR_STATE_BOOL_RESET(fixed_w, false)
-EDITOR_STATE_BOOL_RESET(fill_smooth, true)
-EDITOR_STATE_BOOL_RESET(visible, true)
-EDITOR_STATE_BOOL_RESET(container_min_h, false)
-EDITOR_STATE_BOOL_RESET(container_min_v, false)
-EDITOR_STATE_BOOL_RESET(text_fit_x, false)
-EDITOR_STATE_BOOL_RESET(text_fit_y, false)
-EDITOR_STATE_BOOL_RESET(text_max_x, false)
-EDITOR_STATE_BOOL_RESET(text_max_y, false)
-EDITOR_STATE_BOOL_RESET(text_min_x, false)
-EDITOR_STATE_BOOL_RESET(text_min_y, false)
+EDITOR_STATE_SIMPLE_RESET(fixed_h, false)
+EDITOR_STATE_SIMPLE_RESET(fixed_w, false)
+EDITOR_STATE_SIMPLE_RESET(fill_smooth, true)
+EDITOR_STATE_SIMPLE_RESET(visible, true)
+EDITOR_STATE_SIMPLE_RESET(container_min_h, false)
+EDITOR_STATE_SIMPLE_RESET(container_min_v, false)
+EDITOR_STATE_SIMPLE_RESET(text_fit_x, false)
+EDITOR_STATE_SIMPLE_RESET(text_fit_y, false)
+EDITOR_STATE_SIMPLE_RESET(text_max_x, false)
+EDITOR_STATE_SIMPLE_RESET(text_max_y, false)
+EDITOR_STATE_SIMPLE_RESET(text_min_x, false)
+EDITOR_STATE_SIMPLE_RESET(text_min_y, false)
 
 #define EDITOR_NULL_STRING_DEFAULT_CHECK(FUNC, PROTO_ARGS, ARGS) \
 Eina_Bool \
@@ -154,18 +122,9 @@ editor_##FUNC##_default_is(Evas_Object *edit_object, PROTO_ARGS) \
    return res; \
 }
 
-#define EDITOR_NULL_STRING_RESET(FUNC, PROTO_ARGS, ARGS, RESET_VAL) \
-Eina_Bool \
-editor_##FUNC##_reset(Evas_Object *edit_object, Change *change, PROTO_ARGS) \
-{ \
-   assert(edit_object != NULL); \
-   if (editor_##FUNC##_default_is(edit_object, ARGS)) return true; \
-   return editor_##FUNC##_set(edit_object, change, false, ARGS, RESET_VAL); \
-}
-
 #define EDITOR_STATE_NULL_STRING_RESET(FUNC, RESET_VAL) \
 EDITOR_NULL_STRING_DEFAULT_CHECK(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS) \
-EDITOR_NULL_STRING_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, RESET_VAL)
+EDITOR_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, RESET_VAL)
 
 EDITOR_STATE_NULL_STRING_RESET(rel1_to_x, NULL)
 EDITOR_STATE_NULL_STRING_RESET(rel1_to_y, NULL)
@@ -208,30 +167,7 @@ EDITOR_STATE_INT_INT_INT_INT_RESET(color2, 0, 0, 0, 255)
 EDITOR_STATE_INT_INT_INT_INT_RESET(color3, 0, 0, 0, 128)
 EDITOR_STATE_INT_INT_INT_INT_RESET(image_border, 0, 0, 0, 0)
 
-
-#define EDITOR_UCHAR_DEFAULT_CHECK(FUNC, PROTO_ARGS, ARGS, DEF_VAL) \
-Eina_Bool \
-editor_##FUNC##_default_is(Evas_Object *edit_object, PROTO_ARGS) \
-{ \
-   assert(edit_object != NULL); \
-   unsigned char val = edje_edit_##FUNC##_get(edit_object, ARGS); \
-   return val == DEF_VAL; \
-}
-
-#define EDITOR_UCHAR_RESET(FUNC, PROTO_ARGS, ARGS, RESET_VAL) \
-Eina_Bool \
-editor_##FUNC##_reset(Evas_Object *edit_object, Change *change, PROTO_ARGS) \
-{ \
-   assert(edit_object != NULL); \
-   if (editor_##FUNC##_default_is(edit_object, ARGS)) return true; \
-   return editor_##FUNC##_set(edit_object, change, false, ARGS, RESET_VAL); \
-}
-
-#define EDITOR_STATE_UCHAR_RESET(FUNC, DEF_VAL) \
-EDITOR_UCHAR_DEFAULT_CHECK(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL) \
-EDITOR_UCHAR_RESET(state_##FUNC, EDITOR_STATE_ARGS_PROTO, EDITOR_STATE_ARGS, DEF_VAL)
-
-EDITOR_STATE_UCHAR_RESET(image_border_fill, 0)
-EDITOR_STATE_UCHAR_RESET(fill_type, 0)
-EDITOR_STATE_UCHAR_RESET(aspect_pref, 0)
-EDITOR_STATE_UCHAR_RESET(table_homogeneous, 0)
+EDITOR_STATE_SIMPLE_RESET(image_border_fill, 0)
+EDITOR_STATE_SIMPLE_RESET(fill_type, 0)
+EDITOR_STATE_SIMPLE_RESET(aspect_pref, 0)
+EDITOR_STATE_SIMPLE_RESET(table_homogeneous, 0)
