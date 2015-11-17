68d354a644056f8d3ce22697207274d982a5154d
Vitalii Vorobiov <vi.vorobiov@samsung.com>
2015-11-17 14:21:09 +0200

    image_editor: cleanup old code
    Groups were deleted from grid of images
    so we dont need any sizings anymore

--- src/bin/ui/editors/image_editor.c
+++ src/bin/ui/editors/image_editor.c
@@ -25,8 +25,6 @@ TODO("Rename this file to image_manager")
 
 #define ITEM_WIDTH 100
 #define ITEM_HEIGHT 115
-#define GROUP_ITEM_WIDTH 36
-#define GROUP_ITEM_HEIGHT 36
 #define IMG_EDIT_KEY "image_editor_key"
 
 typedef struct _Image_Editor Image_Editor;
@@ -520,7 +518,6 @@ image_editor_window_add(Image_Editor_Mode mode)
    elm_object_part_content_set(img_edit->layout,
                                "eflete.swallow.grid", img_edit->gengrid);
    elm_gengrid_item_size_set(img_edit->gengrid, ITEM_WIDTH, ITEM_HEIGHT);
-   elm_gengrid_group_item_size_set(img_edit->gengrid, GROUP_ITEM_WIDTH, GROUP_ITEM_HEIGHT);
    elm_gengrid_align_set(img_edit->gengrid, 0.0, 0.0);
    elm_scroller_policy_set(img_edit->gengrid, ELM_SCROLLER_POLICY_OFF,
                            ELM_SCROLLER_POLICY_OFF);
@@ -649,6 +646,4 @@ image_editor_file_choose(Evas_Object *win, const char *selected)
 
 #undef ITEM_WIDTH
 #undef ITEM_HEIGHT
-#undef GROUP_ITEM_WIDTH
-#undef GROUP_ITEM_HEIGHT
 #undef IMG_EDIT_KEY
