1b99ac8d1f5089b49892110d7e39c55e12d02274
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2015-12-31 09:36:08 +0200

    menu: take workspace from tabs correctly
    Change-Id: I3124d4787cdaf0c1d708d38091d61cde20e4d06d

--- src/bin/ui/menu.c
+++ src/bin/ui/menu.c
@@ -134,36 +134,36 @@ _menu_cb(void *data __UNUSED__,
          break;
       case MENU_VIEW_WORKSPACE_ZOOM_IN:
            {
-              double current_factor = workspace_zoom_factor_get(ap.workspace);
-              workspace_zoom_factor_set(ap.workspace, current_factor + 0.1);
+              double current_factor = workspace_zoom_factor_get(tabs_current_workspace_get());
+              workspace_zoom_factor_set(tabs_current_workspace_get(), current_factor + 0.1);
            }
          break;
       case MENU_VIEW_WORKSPACE_ZOOM_OUT:
            {
-              double current_factor = workspace_zoom_factor_get(ap.workspace);
-              workspace_zoom_factor_set(ap.workspace, current_factor - 0.1);
+              double current_factor = workspace_zoom_factor_get(tabs_current_workspace_get());
+              workspace_zoom_factor_set(tabs_current_workspace_get(), current_factor - 0.1);
            }
          break;
       case MENU_VIEW_WORKSPACE_SEPARATE:
            {
-              Eina_Bool sep = workspace_separate_mode_get(ap.workspace);
-              workspace_separate_mode_set(ap.workspace, !sep);
+              Eina_Bool sep = workspace_separate_mode_get(tabs_current_workspace_get());
+              workspace_separate_mode_set(tabs_current_workspace_get(), !sep);
            }
          break;
       case MENU_VIEW_RULERS_SHOW:
-         evas_object_smart_callback_call(ap.workspace, "ruler,toggle", strdup("rulers"));
+         evas_object_smart_callback_call(tabs_current_workspace_get(), "ruler,toggle", strdup("rulers"));
          break;
       case MENU_VIEW_RULERS_ABS:
-         evas_object_smart_callback_call(ap.workspace, "ruler,toggle", strdup("abs"));
+         evas_object_smart_callback_call(tabs_current_workspace_get(), "ruler,toggle", strdup("abs"));
          break;
       case MENU_VIEW_RULERS_REL:
-         evas_object_smart_callback_call(ap.workspace, "ruler,toggle", strdup("rel"));
+         evas_object_smart_callback_call(tabs_current_workspace_get(), "ruler,toggle", strdup("rel"));
          break;
       case MENU_VIEW_RULERS_BOTH:
-         evas_object_smart_callback_call(ap.workspace, "ruler,toggle", strdup("abs&rel"));
+         evas_object_smart_callback_call(tabs_current_workspace_get(), "ruler,toggle", strdup("abs&rel"));
          break;
       case MENU_VIEW_WORKSPACE_OBJECT_AREA:
-         evas_object_smart_callback_call(ap.workspace, "highlight,visible", NULL);
+         evas_object_smart_callback_call(tabs_current_workspace_get(), "highlight,visible", NULL);
          break;
       case MENU_EDITORS_ANIMATOR:
            {
