639f6250ba99bcddbbd96a05060b787809c9c6cf
Vitalii Vorobiov <vi.vorobiov@samsung.com>
2016-01-06 14:23:05 +0000

    demo: list of signals available to send into demo object

--- src/bin/ui/demo_group.h
+++ src/bin/ui/demo_group.h
@@ -59,6 +59,12 @@ typedef enum _Swallow_Content_Widget_Type
  */
 typedef struct Demo_Part_ Demo_Part;
 
+/**
+ * @typedef Demo_Signal_
+ * @ingroup DemoGroup
+ */
+typedef struct Demo_Signal_ Demo_Signal;
+
 struct Demo_Part_
 {
    Eina_Stringshare *name;    /**< part name */
@@ -73,6 +79,13 @@ struct Demo_Part_
    int max_w, max_h, min_w, min_h; /**< object's size */
    Eina_Bool change; /**< in case when content type is changed,
                           demo need to create new one */
+};
+
+struct Demo_Signal_
+{
+   Eina_Stringshare *prog_name;
+   Eina_Stringshare *sig_name;
+   Eina_Stringshare *source_name;
 };
 
 /**
