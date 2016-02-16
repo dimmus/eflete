1c0426286fd6379ea23eedce96b36b02ebeca9e6
Andrii Kroitor <an.kroitor@samsung.com>
2016-02-16 15:25:32 +0200

    property: start from scratch

--- src/bin/ui/property/property.c
+++ src/bin/ui/property/property.c
@@ -1,6 +1,6 @@
 /*
  * Edje Theme Editor
- * Copyright (C) 2013-2014 Samsung Electronics.
+ * Copyright (C) 2013-2016 Samsung Electronics.
  *
  * This file is part of Edje Theme Editor.
  *
@@ -17,27 +17,10 @@
  * along with this program; If not, see www.gnu.org/licenses/lgpl.html.
  */
 
-#ifndef UI_PROPERTY_1_H
-#define UI_PROPERTY_1_H
+#include "property.h"
 
-/**
- * @defgroup Property Property
- * @ingroup Block
- *
- * This widget designed for show group and part details.
- */
-
-#include "eflete.h"
-
-/**
- * Add a new Property widget.
- *
- * @param parent The parent widget.
- * @return A new 'property' widget handler or NULL, on errors
- *
- * @ingroup Property
- */
 Evas_Object *
-ui_property_add(Evas_Object *parent);
-
-#endif /* UI_PROPERTY_1_H */
+property_add(Evas_Object *parent __UNUSED__)
+{
+   return NULL;
+}
