c22ce8d742ff9efbfe9021739186050e350c7f5c
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2016-02-24 14:32:39 +0200

    workspace: add the core structure and start implement workspace_add
    Change-Id: I707b3e2bccccc9ce59877f58e738c1d20ca3e1f4

--- src/bin/ui/workspace/workspace.c
+++ src/bin/ui/workspace/workspace.c
@@ -1,6 +1,6 @@
 /*
  * Edje Theme Editor
- * Copyright (C) 2013-2014 Samsung Electronics.
+ * Copyright (C) 2013-2016 Samsung Electronics.
  *
  * This file is part of Edje Theme Editor.
  *
@@ -30,6 +30,10 @@
 #include "project_manager.h"
 #include "change.h"
 
+#define WORKSPACE_DATA "workspace_data"
+
+#define PANES_RIGHT_SIZE_MIN 225
+
 typedef enum
 {
    MODE_NORMAL = 1,
@@ -39,6 +43,31 @@ typedef enum
    MODE_DEMO,
    MODE_LAST
 } Workspace_Mode;
+
+struct _Workspace_Data
+{
+   Evas_Object *panes; /* equal to all workspace, this object returned in workspace_add */
+   Evas_Object *group_navi;
+   Evas_Object *demo_navi;
+   Evas_Object *toolbar;
+   Evas_Object *panes_h; /* for set subobject like code, sequance etc */
+   Evas_Object *workspace_layout; /* layout for rulers and scroller */
+   Evas_Object *ruler_v, *ruler_h;
+   struct {
+      Evas_Object *scroller;
+      Evas_Object *container;
+      Evas_Object *bg;
+      Evas_Object *content; /* for normal mode - groupview, for demo - elm widget */
+   } normal, demo;
+   struct {
+      Evas_Object *obj;
+      double size;
+   };
+   Workspace_Mode mode;
+   Group *group;
+};
+
+typedef struct _Workspace_Data Workspace_Data;
 
 Eina_Bool
 workspace_zoom_factor_set(Evas_Object *obj __UNUSED__, double factor __UNUSED__)
@@ -65,10 +94,48 @@ workspace_active_demo_mode_get(Evas_Object *obj __UNUSED__)
    return false;
 }
 
-Evas_Object *
-workspace_add(Evas_Object *parent __UNUSED__, Group *group __UNUSED__)
+static void
+_job_workspace_del(void *data)
 {
-   return NULL;
+   assert(data != NULL);
+
+   Workspace_Data *wd = data;
+   gm_group_edit_object_unload(wd->group);
+
+   free(wd);
+}
+
+static void
+_workspace_del(void *data,
+               Evas *e __UNUSED__,
+               Evas_Object *obj __UNUSED__,
+               void *event_info __UNUSED__)
+{
+   ecore_job_add(_job_workspace_del, data);
+}
+
+Evas_Object *
+workspace_add(Evas_Object *parent, Group *group)
+{
+   Workspace_Data *wd;
+
+   assert(parent != NULL);
+   assert(group != NULL);
+
+   /* load the editable object */
+   gm_group_edit_object_load(ap.project, group, evas_object_evas_get(ap.win));
+   edje_object_animation_set(group->edit_object, false);
+
+   wd = mem_calloc(1, sizeof(Workspace_Data));
+   wd->panes = elm_panes_add(parent);
+   elm_panes_content_right_min_size_set(wd->panes, PANES_RIGHT_SIZE_MIN);
+   elm_panes_content_right_size_set(wd->panes, PANES_RIGHT_SIZE_MIN);
+   wd->group = group;
+
+   evas_object_data_set(wd->panes, WORKSPACE_DATA, wd);
+   evas_object_event_callback_add(wd->panes, EVAS_CALLBACK_DEL, _workspace_del, wd);
+
+   return wd->panes;
 }
 
 void
