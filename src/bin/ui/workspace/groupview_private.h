c10e5dec9f3c728a5e91dea1fbd087e41038f899
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2016-03-09 08:16:33 +0200

    groupview: delete arhaic signal, which not used anymore
    Change-Id: Idd3330b2a10155a96482dd5a177adec9e6c271e6

--- src/bin/ui/workspace/groupview_private.h
+++ src/bin/ui/workspace/groupview_private.h
@@ -25,7 +25,6 @@ typedef struct _Groupview_Item Groupview_Item;
 
 static const char SIG_PART_SEPARETE_OPEN[] = "parts,separete,open";
 static const char SIG_PART_SEPARETE_CLOSE[] = "parts,separete,close";
-static const char SIG_PART_UNSELECTED[] = "part,unselected";
 static const char SIG_OBJ_AREA_CHANGED[] = "object,area,changed";
 static const char SIG_GEOMETRY_CHANGED[] = "geometry,changed";
 static const char TEXT_TOOLTIP[] = "gs.current.size.tooltip";
@@ -37,7 +36,6 @@ static const Evas_Smart_Cb_Description _smart_callbacks[] = {
    /* about types see here: http://docs.enlightenment.org/auto/evas/group__Evas__Keys.html#gaf0d4ce3d62a068eab1b89a34abb056ad */
    {SIG_PART_SEPARETE_OPEN, ""},
    {SIG_PART_SEPARETE_CLOSE, ""},
-   {SIG_PART_UNSELECTED, "s"},
    {SIG_OBJ_AREA_CHANGED, "(iiii)"},
    {SIG_GEOMETRY_CHANGED, "(iiii)"},
    {NULL, NULL}
