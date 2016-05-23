8c4f75bb1f9f8c4bf04af13f6a1bf64ff7b84234
Vitalii Vorobiov <vi.vorobiov@samsung.com>
2016-05-23 21:47:25 +0300

    property_textblock: parse and set clicked style

--- src/bin/ui/property/property_textblock.c
+++ src/bin/ui/property/property_textblock.c
@@ -20,11 +20,22 @@
 #include "property.h"
 #include "property_private.h"
 
+#define FONT_DEFAULT "DEFAULT='align=middle font=Sans font_size=24 color=#000000 "
+
 struct _Property_Textblock_Data {
+   Eina_Stringshare *font;
+   int font_size;
+   int font_style_weight;
+   int font_style_width;
+   struct {
+      int r, g, b, a;
+   } color;
+
    Property_Attribute items[PROPERTY_TEXTBLOCK_ITEM_LAST];
 };
 typedef struct _Property_Textblock_Data Property_Textblock_Data;
 static Property_Textblock_Data textblock_pd;
+#define tpd textblock_pd
 
 struct _Property_Textblock_Update_Info {
    Property_Attribute *pa;
@@ -34,6 +45,160 @@ typedef struct _Property_Textblock_Update_Info Property_Textblock_Update_Info;
 
 /* array to find item by Attribute */
 static Property_Textblock_Update_Info attribute_map[ATTRIBUTE_TEXTBLOCK_ITEM_LAST];
+
+static const char *font_weight_list[] = { "Normal",
+                                          "Thin",
+                                          "Ultralight",
+                                          "Light",
+                                          "Book",
+                                          "Medium",
+                                          "Semibold",
+                                          "Bold",
+                                          "Ultrabold",
+                                          "Black",
+                                          "Extrablack",
+                                          NULL};
+
+static const char *font_width_list[] = { "Normal",
+                                         "Ultracondensed",
+                                         "Extracondensed",
+                                         "Condensed",
+                                         "Extracondensed",
+                                         "Semicondensed",
+                                         "Semiexpanded",
+                                         "Expanded",
+                                         "Ultraexpanded",
+                                         NULL};
+
+
+/************************************************************************/
+/*************        LOCAL FUNCTIONS TO SPLIT STYLE         ************/
+/************************************************************************/
+
+static int
+_combobox_get_num(Eina_Tmpstr *tmp, const char **array)
+{
+   int i = 0;
+
+   assert(tmp != NULL);
+   assert(array != NULL);
+
+   while (array[i] != NULL)
+     {
+        if (strcmp(tmp, array[i])) return i;
+        ++i;
+     }
+   return 0; /* NONE */
+}
+
+static Eina_Bool
+_hex_to_rgb(const char *hex, int *r, int *g, int *b, int *a)
+{
+   assert(hex != NULL);
+   assert(r != NULL);
+   assert(g != NULL);
+   assert(b != NULL);
+   assert(a != NULL);
+
+   unsigned long val;
+   char *end;
+   int length = (int)strlen(hex);
+   switch (length)
+     {
+      case 7:
+        {
+           val = strtoul(hex + 1, &end, 16);
+           *r = (val & 0xff0000) >> 16;
+           *g = (val & 0xff00) >> 8;
+           *b = val & 0xff;
+           *a = 255;
+           break;
+        }
+      case 9:
+        {
+           val = strtoul(hex + 1, &end, 16);
+           *r = (val & 0xff000000) >> 24;
+           *g = (val & 0xff0000) >> 16;
+           *b = (val & 0xff00) >> 8;
+           *a = val & 0xff;
+           break;
+        }
+      case 5:
+        {
+           val = strtoul(hex + 1, &end, 16);
+           *r = (val & 0xf000) >> 12;
+           *g = (val & 0xf00) >> 8;
+           *b = (val & 0xf0) >> 4;
+           *a = val & 0xf;
+           *r = (*r << 4) | *r;
+           *g = (*g << 4) | *g;
+           *b = (*b << 4) | *b;
+           *a = (*a << 4) | *a;
+           break;
+        }
+      case 4:
+        {
+           val = strtoul(hex + 1, &end, 16);
+           *r = (val & 0xf00) >> 8;
+           *g = (val & 0xf0) >> 4;
+           *b = val & 0xf;
+           *a = 255;
+           *r = (*r << 4) | *r;
+           *g = (*g << 4) | *g;
+           *b = (*b << 4) | *b;
+           break;
+        }
+      default:
+        {
+           *r = 0;
+           *g = 0;
+           *b = 0;
+           *a = 255;
+        }
+     }
+   return true;
+}
+
+static Eina_Tmpstr*
+_tag_value_get(const char* text_style, char* a_tag)
+{
+   assert(text_style != NULL);
+   assert(a_tag != NULL);
+
+   char *tag_list_copy = mem_malloc(strlen(text_style) + 1);
+   Eina_Tmpstr *result = NULL;
+   char *token;
+
+   strcpy(tag_list_copy, text_style);
+   token = strtok(tag_list_copy, " ");
+
+   while (token)
+     {
+        char* equals_sign = strchr(token, '=');
+        if (equals_sign)
+          {
+             *equals_sign = 0;
+             if (!strcmp(token, a_tag))
+               {
+                  equals_sign++;
+
+                  if (equals_sign[strlen(equals_sign) - 1] != '\'')
+                    result = eina_tmpstr_add(equals_sign);
+                  else
+                    result = eina_tmpstr_add_length(equals_sign, strlen(equals_sign) - 1);
+                  if (!strstr(FONT_DEFAULT, a_tag)) break;
+               }
+          }
+        token = strtok(0, " ");
+     }
+   free(tag_list_copy);
+
+   return result;
+}
+
+/************************************************************************/
+/************************************************************************/
+/************************************************************************/
 
 /* local callbacks */
 static Eina_List *
@@ -249,6 +414,61 @@ _init_items()
 #undef IT
 }
 
+static void
+_on_style_selected(void *data __UNUSED__,
+                   Evas_Object *obj __UNUSED__,
+                   void *event_info)
+{
+   Style_Data *current_style = (Style_Data *)event_info;
+   Eina_Tmpstr *tmp;
+   const char *value;
+
+   if (current_style)
+     {
+        /* do a split */
+        value = eina_strbuf_string_get(current_style->style);
+
+        tmp = _tag_value_get(value, "font");
+        if (!tmp) tmp = eina_tmpstr_add("");
+        if (tpd.font) eina_stringshare_del(tpd.font);
+        tpd.font = eina_stringshare_add(tmp);
+        eina_tmpstr_del(tmp);
+
+        tmp = _tag_value_get(value, "font_size");
+        if (!tmp) tmp = eina_tmpstr_add("");
+        tpd.font_size = atof(tmp);
+        eina_tmpstr_del(tmp);
+
+        tmp = _tag_value_get(value, "font_weight");
+        if (!tmp) tmp = eina_tmpstr_add("Normal");
+        tpd.font_style_weight = _combobox_get_num(tmp, font_weight_list);
+        eina_tmpstr_del(tmp);
+
+        tmp = _tag_value_get(value, "font_width");
+        if (!tmp) tmp = eina_tmpstr_add("Normal");
+        tpd.font_style_width = _combobox_get_num(tmp, font_width_list);
+        eina_tmpstr_del(tmp);
+
+        tmp = _tag_value_get(value, "color");
+        if (!_hex_to_rgb(tmp, &tpd.color.r, &tpd.color.g, &tpd.color.b, &tpd.color.a))
+          {
+             ERR("Can't convert color value");
+             abort();
+          }
+        eina_tmpstr_del(tmp);
+     }
+   else
+     {
+        if (tpd.font)
+          eina_stringshare_del(tpd.font);
+        tpd.font = NULL;
+     }
+
+   property_item_update_recursively(&textblock_pd.items[PROPERTY_TEXTBLOCK_ITEM_TEXT_TITLE]);
+}
+
+#undef tpd
+
 /* public */
 void
 property_textblock_manager_init(Property_Data *pd)
@@ -256,6 +476,8 @@ property_textblock_manager_init(Property_Data *pd)
    assert(pd != NULL);
 
    _init_items();
+
+   evas_object_smart_callback_add(ap.win, SIGNAL_STYLE_SELECTED, _on_style_selected, NULL);
 }
 
 Eina_List *
