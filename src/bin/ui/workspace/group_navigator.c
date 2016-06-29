7fc3e065253c0700b933c3acad32a0bbf5d5c168
Mykyta Biliavskyi <m.biliavskyi@samsung.com>
2016-06-29 14:03:12 +0300

    Group navigator: use unified popup API for part state dialog.
    Use popup_want_action for manage "part state add" dialog.

--- src/bin/ui/workspace/group_navigator.c
+++ src/bin/ui/workspace/group_navigator.c
@@ -1245,6 +1245,13 @@ _popup_add_state_ok_clicked(void *data,
    ecore_job_add(_job_popup_del, pl);
 }
 
+Eina_Bool
+_popup_add_state_validator(void *data)
+{
+   _popup_add_state_ok_clicked(data, NULL, NULL);
+   return true;
+}
+
 void
 group_navigator_part_state_add(Evas_Object *obj, Part *part, State *state)
 {
@@ -1282,31 +1289,19 @@ group_navigator_part_state_add(Evas_Object *obj, Part *part, State *state)
      }
 }
 
-static void
-_on_menu_add_state_clicked(void *data __UNUSED__,
-                           Evas_Object *obj,
-                           void *ei __UNUSED__)
+Evas_Object *
+_add_state_content_get(void *data)
 {
-   Part_List *pl = evas_object_data_get(obj, GROUP_NAVIGATOR_DATA);
-   State *state;
-   Eina_Stringshare *title;
+   Part_List *pl = (Part_List *)data;
    Evas_Object *box, *item;
    Eina_List *l;
    Eina_Stringshare *label;
-
-   assert(pl != NULL);
-   assert(pl->part != NULL);
-
-   ap.popup = elm_popup_add(ap.win);
-   elm_popup_orient_set(ap.popup, ELM_POPUP_ORIENT_CENTER);
-   title = eina_stringshare_printf(_("Add New State to Part \"%s\""), pl->part->name);
-   elm_object_part_text_set(ap.popup, "title,text", title);
-   eina_stringshare_del(title);
+   State *state = NULL;
 
    BOX_ADD(ap.popup, box, false, false);
    elm_box_padding_set(box, 0, 10);
 
-   LAYOUT_PROP_ADD(box, _("State name"), "popup", "1swallow")
+   LAYOUT_PROP_ADD(box, _("State name"), "popup", "1swallow");
    ENTRY_ADD(item, pl->popup.entry_name, true);
    eo_event_callback_add(pl->popup.entry_name, ELM_ENTRY_EVENT_VALIDATE,
                          elm_validator_regexp_helper, pl->name_validator);
@@ -1339,20 +1334,29 @@ _on_menu_add_state_clicked(void *data __UNUSED__,
    elm_object_part_content_set(item, "elm.swallow.content", pl->popup.combobox);
 
    elm_box_pack_end(box, item);
-   elm_object_content_set(ap.popup, box);
 
-   BUTTON_ADD(ap.popup, pl->popup.btn_add, _("Add"));
-   evas_object_smart_callback_add(pl->popup.btn_add, "clicked", _popup_add_state_ok_clicked, pl);
-   elm_object_part_content_set(ap.popup, "button1", pl->popup.btn_add);
-   elm_object_disabled_set(pl->popup.btn_add, true);
+   pl->popup.box = box;
+   return box;
+}
 
-   BUTTON_ADD(ap.popup, pl->popup.btn_cancel, _("Cancel"));
-   evas_object_smart_callback_add(pl->popup.btn_cancel, "clicked", _popup_cancel_clicked, pl);
-   elm_object_part_content_set(ap.popup, "button2", pl->popup.btn_cancel);
-   ui_menu_items_list_disable_set(ap.menu, MENU_ITEMS_LIST_MAIN, true);
-   ui_menu_items_list_disable_set(ap.menu, MENU_ITEMS_LIST_MAIN, true);
-   elm_object_focus_set(pl->popup.entry_name, true);
-   evas_object_show(ap.popup);
+static void
+_on_menu_add_state_clicked(void *data __UNUSED__,
+                           Evas_Object *obj,
+                           void *ei __UNUSED__)
+{
+   Part_List *pl = evas_object_data_get(obj, GROUP_NAVIGATOR_DATA);
+   Eina_Stringshare *title;
+   assert(pl != NULL);
+   assert(pl->part != NULL);
+
+   title = eina_stringshare_printf(_("Add New State to Part \"%s\""), pl->part->name);
+   Popup_Button button = popup_want_action(title, NULL, _add_state_content_get,
+                                           NULL, BTN_OK | BTN_CANCEL,
+                                           _popup_add_state_validator, pl);
+
+   if (button == BTN_CANCEL)
+     evas_object_del(pl->popup.box);
+   eina_stringshare_del(title);
 }
 
 static void
