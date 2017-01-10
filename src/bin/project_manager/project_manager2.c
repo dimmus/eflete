cfd82661a7af3f1622ce5b4c8fe83ab24ef1daa0
Andrii Kroitor <an.kroitor@samsung.com>
2017-01-10 11:52:26 +0200

    project_manager: add missing argument to open call
    Change-Id: If209d19a15a010f321b1377194dcffb0e6563e29

--- src/bin/project_manager/project_manager2.c
+++ src/bin/project_manager/project_manager2.c
@@ -424,7 +424,9 @@ _project_lock(Project *project)
    dir = ecore_file_dir_get(project->pro_path);
    snprintf(path, sizeof(path), "%s/"LOCK_FILE, dir);
    free(dir);
-   project->fd_lock = open(path, O_RDWR | O_CREAT, S_IROTH | S_IWOTH);
+   project->fd_lock = open(path,
+                           O_RDWR | O_CREAT,
+                           S_IREAD | S_IWRITE | S_IRGRP | S_IROTH); /* rw-r--r-- */
    if (!project->fd_lock)
      {
         ERR("%s: %s\n", path, strerror(errno));
