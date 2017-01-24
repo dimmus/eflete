feac422ce5cd3a31edd4b0bec5739005e878d6d4
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2017-01-24 15:06:07 +0200

    tabs: don't unset the property if home tab is selected
    Change-Id: I6cce7dea413c168b56b83eb6283581193af9f473

--- src/bin/ui/tabs.c
+++ src/bin/ui/tabs.c
@@ -100,7 +100,6 @@ _content_set(void *data,
         if (!item->group)
           {
              elm_object_part_content_set(ap.panes.left_ver, "right", workspace_group_navigator_get(NULL));
-             elm_object_part_content_unset(ap.panes.right, "right");
              /* this case for when Property shows something selected in NORMAL mode */
              evas_object_smart_callback_call(ap.win, SIGNAL_GROUP_CHANGED, NULL);
              /* this case for when Property shows something selected in DEMO mode */
@@ -1121,7 +1120,6 @@ _tab_close(void *data,
         content = elm_layout_content_unset(ap.panes.left_ver, "right");
         evas_object_hide(content);
         elm_layout_content_set(ap.panes.left_ver, "right", workspace_group_navigator_get(NULL));
-        elm_object_part_content_unset(ap.panes.right, "right");
         /* this case for when Property shows something selected in NORMAL mode */
         evas_object_smart_callback_call(ap.win, SIGNAL_GROUP_CHANGED, NULL);
         /* this case for when Property shows something selected in DEMO mode */
