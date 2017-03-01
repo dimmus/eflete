6490bebdb2bf66f690d886711d11b892fefde11c
Tetiana Naumenko <t.naumenko@samsung.com>
2017-03-01 12:51:57 +0200

    script manager: add script compilation
    Implement callback for button "Save" that designed to show result of
    script compilation after it was chenged and if result is "success" save
    script
    
    Change-Id: I8861f92d8357276cf4948baaa8889deaf769043c

--- src/bin/ui/script_manager.c
+++ src/bin/ui/script_manager.c
@@ -30,9 +30,11 @@ struct _Script_Manager
 {
    Evas_Object *win;
    Evas_Object *panes;
-   Evas_Object *entry_prev;
+   Evas_Object *script_entry;
+   Evas_Object *error_entry;
    Evas_Object *image;
-   Evas_Object *layout;
+   Evas_Object *script_layout;
+   Evas_Object *error_layout;
    Evas_Object *menu;
 };
 
@@ -62,10 +64,73 @@ _mw_done_cb(void *data __UNUSED__,
    evas_object_hide(content);
 }
 
+static void
+_btn_save_cb(void *data,
+            Evas_Object *obj __UNUSED__,
+            void *event_info __UNUSED__)
+{
+   Resource2 *res = data;
+   Eina_Stringshare *code;
+   Group2 *group;
+   Program2 *program;
+   Eina_List *errors = NULL;
+   Eina_List *l = NULL;
+   Edje_Edit_Script_Error *list_error = NULL;
+
+   assert(res != NULL);
+   assert((res->common.type == RESOURCE2_TYPE_GROUP) ||
+          (res->common.type == RESOURCE2_TYPE_PROGRAM));
+   assert(mng.win != NULL);
+
+   code = property_entry_get(mng.script_entry);
+
+   if (res->common.type == RESOURCE2_TYPE_GROUP)
+     {
+        program = NULL;
+        group = (Group2 *)res;
+     }
+   else
+     {
+        program = (Program2 *)res;
+        group = program->group;
+     }
+
+   if (res->common.type == RESOURCE2_TYPE_GROUP)
+     {
+        edje_edit_script_set(group->edit_object, code);
+     }
+   else
+     {
+        edje_edit_script_program_set(group->edit_object, program->common.name, code);
+     }
+
+   if (!edje_edit_script_compile(group->edit_object))
+     {
+        errors = (Eina_List *)edje_edit_script_error_list_get(group->edit_object);
+        if (errors != NULL)
+          {
+             EINA_LIST_FOREACH(errors, l, list_error)
+               {
+                  list_error = (Edje_Edit_Script_Error *)errors->data;
+                  property_entry_set(mng.error_entry, (char *)list_error->error_str);
+               }
+          }
+        else
+           property_entry_set(mng.error_entry, _("ERROR!"));
+     }
+   else
+     {
+        property_entry_set(mng.error_entry, _("SUCCESS!"));
+        CRIT_ON_FAIL(editor_save(group->edit_object));
+     }
+
+   eina_stringshare_del(code);
+}
+
 Evas_Object *
 script_manager_add(Resource2 *res)
 {
-   Evas_Object *ic, *script_entry, *search_entry;
+   Evas_Object *ic, *search_entry, *btn_save;
    char *code;
    Group2 *group;
    Program2 *program;
@@ -88,13 +153,28 @@ script_manager_add(Resource2 *res)
 #endif
    mw_icon_set(mng.win, ic);
 
-   mng.layout = elm_layout_add(mng.win);
-   elm_layout_theme_set(mng.layout, "layout", "script_manager", "default");
+   mng.script_layout = elm_layout_add(mng.win);
+   elm_layout_theme_set(mng.script_layout, "layout", "script_manager", "default");
+   mng.error_layout = elm_layout_add(mng.win);
+   elm_layout_theme_set(mng.error_layout, "layout", "script_manager", "build_log");
+   elm_layout_text_set(mng.error_layout, "elm.text", "Build log");
 
-   ENTRY_ADD(mng.layout, script_entry, true);
-   elm_entry_single_line_set(script_entry, false);
-   elm_object_content_set(mng.layout, script_entry);
-   elm_layout_content_set(mng.layout, "elm.swallow.list", script_entry);
+   mng.panes = elm_panes_add(mng.win);
+   elm_object_part_content_set(mng.panes, "left", mng.script_layout);
+   elm_object_part_content_set(mng.panes, "right", mng.error_layout);
+   elm_panes_content_right_size_set(mng.panes, 0.25);
+
+   ENTRY_ADD(mng.script_layout, mng.script_entry, true);
+   elm_entry_single_line_set(mng.script_entry, false);
+   elm_object_content_set(mng.script_layout, mng.script_entry);
+   elm_layout_content_set(mng.script_layout, "elm.swallow.list", mng.script_entry);
+
+   ENTRY_ADD(mng.error_layout, mng.error_entry, true);
+   elm_entry_single_line_set(mng.error_entry, false);
+   elm_entry_editable_set(mng.error_entry, false);
+   elm_entry_line_wrap_set(mng.error_entry, ELM_WRAP_WORD);
+   elm_object_content_set(mng.error_layout, mng.error_entry);
+   elm_layout_content_set(mng.error_layout, "elm.swallow.content", mng.error_entry);
 
    if (res->common.type == RESOURCE2_TYPE_GROUP)
      {
@@ -116,10 +196,13 @@ script_manager_add(Resource2 *res)
         code = edje_edit_script_program_get(group->edit_object, program->common.name);
      }
 
-   property_entry_set(script_entry, code);
-   free(code);
+   property_entry_set(mng.script_entry, code);
 
-   ENTRY_ADD(mng.layout, search_entry, true);
+   BUTTON_ADD(mng.win, btn_save, _("Save"))
+   evas_object_smart_callback_add(btn_save, signals.elm.button.clicked, _btn_save_cb, res);
+   elm_object_part_content_set(mng.win, "eflete.swallow.btn_ok", btn_save);
+
+   ENTRY_ADD(mng.script_layout, search_entry, true);
    elm_object_part_text_set(search_entry, "guide", _("Search"));
 #if !HAVE_TIZEN
    Evas_Object *icon;
@@ -128,9 +211,10 @@ script_manager_add(Resource2 *res)
 #else
    elm_object_style_set(search_entry, "search");
 #endif
-   elm_layout_content_set(mng.layout, "elm.swallow.search", search_entry);
+   elm_layout_content_set(mng.script_layout, "elm.swallow.search", search_entry);
 
-   elm_object_content_set(mng.win, mng.layout);
+   elm_object_content_set(mng.win, mng.panes);
    evas_object_show(mng.win);
+   free(code);
    return mng.win;
 }
